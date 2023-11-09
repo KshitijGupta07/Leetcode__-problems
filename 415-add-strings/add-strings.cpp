db0ab26fa8fe839b50aecd82980f325f3d5fb869
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2023-11-09 13:07:03 +0530

    Time: 15 ms (21.48%) | Memory: 7.1 MB (59.13%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        if(num1.length()>num2.length()){
+            while(num2.length()!=num1.length()){
+                num2.insert(num2.begin(),'0');
+            }
+            
+        }
+        if(num2.length()>num1.length()){
+            while(num1.length()!=num2.length()){
+                num1.insert(num1.begin(),'0');
+            }
+            
+        }
+        int n=num1.size();
+        cout<<num1<<" "<<num2<<endl;
+        if(n>1){
+        for(int i=0;i<n;i++){
+            num1[i]=(num1[i]-'0'+num2[i]-'0')+'0';
+        }
+    }
+        if(n>1){
+        for(int i=n-1;i>=1;i--){
+            if((num1[i]-'0')/10>=1){
+                num1[i]=((num1[i]-'0')%10)+'0';
+                num1[i-1]=(num1[i-1]-'0'+1)+'0';
+            }
+        }
+        if(num1[0]-'0'>=10){
+            num1[0]=(num1[0]-'0')%10+'0';
+            num1.insert(num1.begin()+0,'1');
+        }
+        return num1;
+        
+    }
+    int ans=(num1[0]-'0')+(num2[0]-'0');
+    string a=to_string(ans);
+    return a;
+        
+    }
+};
