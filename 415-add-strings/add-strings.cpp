e3893effd43ef06e872e4403f69c7bb422c68713
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2023-11-18 12:38:33 +0530

    Time: 55 ms (5.18%) | Memory: 230.9 MB (5.01%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
+    string addRE(string num1,string num2,int p1,int p2,int carry=0){
+        //ek case solve karna
+        if(p1<0&&p2<0){
+            if(carry!=0){
+                return string(1,carry+'0');
+            }
+            return "";
+        }
+        int n1=(p1>=0 ? num1[p1]:'0')-'0';
+        int n2=(p2>=0 ? num2[p2]: '0')-'0';
+        int currentsum=n1+n2+carry;
+         carry=currentsum/10;
+        int digit=currentsum%10;
+        string ans="";
+        ans.push_back(digit+'0');
+       ans+= addRE(num1,num2,p1-1,p2-1,carry);
+       return ans;
+    }
     string addStrings(string num1, string num2) {
-        if(num1.length()>num2.length()){
-            while(num2.length()!=num1.length()){
-                num2.insert(num2.begin(),'0');
-            }
-            
-        }
-        if(num2.length()>num1.length()){
-            while(num1.length()!=num2.length()){
-                num1.insert(num1.begin(),'0');
-            }
-            
-        }
-        
-        
-        if(num1.size()>1){
-        for(int i=0;i<num1.size();i++){
-            num1[i]=(num1[i]-'0'+num2[i]-'0')+'0';
-        }
-    }
-        if(num1.size()>1){
-        for(int i=num1.size()-1;i>=1;i--){
-            if((num1[i]-'0')/10>=1){
-                num1[i]=((num1[i]-'0')%10)+'0';
-                num1[i-1]=(num1[i-1]-'0'+1)+'0';
-            }
-        }
-        if(num1[0]-'0'>=10){
-            num1[0]=(num1[0]-'0')%10+'0';
-            num1.insert(num1.begin()+0,'1');
-        }
-        return num1;
-        
-    }
-    int ans=(num1[0]-'0')+(num2[0]-'0');
-    string a=to_string(ans);
-    return a;
-        
+        //recursively
+        string ans=addRE(num1,num2,num1.size()-1,num2.size()-1);
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
