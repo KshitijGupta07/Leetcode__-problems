c43c77ef1f4301746c4ef1ce176be2d04bb9d5d8
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2023-12-20 13:29:14 +0530

    Time: 60 ms (96.04%) | Memory: 51 MB (46.13%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -9,6 +9,9 @@ public:
         if(nums2[0]>nums1[nums1.size()-1]){
             return -1;
         }
+        if(nums1[0]==nums2[0]){
+            return nums1[0];
+        }
         for(int i=0,j=0;i<nums1.size()&&j<nums2.size();){
             if(nums1[i]==nums2[j]){
                 return nums1[i];
