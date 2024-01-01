da60ddba4cdaa61bf921724bb892fbc3d8969dc7
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-01-01 17:11:07 +0530

    Time: 3 ms (75.14%) | Memory: 11.5 MB (40.11%) - LeetSync

--- 2459-minimum-hours-of-training-to-win-a-competition/minimum-hours-of-training-to-win-a-competition.cpp
+++ 2459-minimum-hours-of-training-to-win-a-competition/minimum-hours-of-training-to-win-a-competition.cpp
@@ -26,11 +26,11 @@ public:
               
             
         }
-        if(initialEnergy>sum&&s==1){
+       else if(initialEnergy>sum&&s==1){
             return max;
             
         }
-        if(initialEnergy<=sum&&s==0){
+        else if(initialEnergy<=sum&&s==0){
              
             return sum-initialEnergy+1;
         }
