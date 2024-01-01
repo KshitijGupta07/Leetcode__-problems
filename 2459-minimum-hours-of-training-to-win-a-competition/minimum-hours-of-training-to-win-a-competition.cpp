61971cbbf2dbe1c83056d17845f0a4c141c8675a
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-01-01 17:09:34 +0530

    Time: 3 ms (75.14%) | Memory: 11.4 MB (80.79%) - LeetSync

---
+++ 2459-minimum-hours-of-training-to-win-a-competition/minimum-hours-of-training-to-win-a-competition.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minNumberOfHours(int initialEnergy, int initialExperience, vector<int>& energy, vector<int>& experience) {
+        int sum=0;
+        int a=0;
+        int max=0;
+        bool s=0;
+        for(int i=0;i<experience.size();i++){
+           if(initialExperience<=experience[i]){
+               s=1;
+               a=experience[i]-initialExperience+1;
+               if(a>max){
+                   max=a;
+               }
+               
+
+               
+           }
+           
+           sum+=energy[i];
+           initialExperience+=experience[i];
+        }
+        cout<<sum-initialEnergy+1<<" "<<a<<" "<<sum<<endl;
+        if(initialEnergy>sum&&s==0){
+        return 0;
+              
+            
+        }
+        if(initialEnergy>sum&&s==1){
+            return max;
+            
+        }
+        if(initialEnergy<=sum&&s==0){
+             
+            return sum-initialEnergy+1;
+        }
+        
+        
+        
+
+      return sum-initialEnergy+1+max;
+    }
+};
