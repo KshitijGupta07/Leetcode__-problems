1797f65fca2e1e758362e5a41ac55e808d500071
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-01-03 14:53:42 +0530

    Time: 4 ms (31.17%) | Memory: 6.5 MB (46.32%) - LeetSync

--- 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.cpp
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.cpp
@@ -3,7 +3,7 @@ public:
     bool isFascinating(int n) {
        string a=to_string(n)+to_string(2*n)+to_string(3*n);
        
-       long long ans=stoll(a);
+       long ans=stoll(a);
        int multi=1;
         while(ans!=0){
           multi=multi*(ans%10);
