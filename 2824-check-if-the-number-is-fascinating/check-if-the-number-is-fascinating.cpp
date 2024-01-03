c11dbb096439f7400a6a69731a1f33bdd3468e4f
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-01-03 14:53:05 +0530

    Time: 2 ms (51.95%) | Memory: 6.5 MB (33.55%) - LeetSync

---
+++ 2824-check-if-the-number-is-fascinating/check-if-the-number-is-fascinating.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isFascinating(int n) {
+       string a=to_string(n)+to_string(2*n)+to_string(3*n);
+       
+       long long ans=stoll(a);
+       int multi=1;
+        while(ans!=0){
+          multi=multi*(ans%10);
+          ans=ans/10;
+        }
+        if(multi==362880){
+            return true;
+        }
+        return false;
+           
+    }
+};
