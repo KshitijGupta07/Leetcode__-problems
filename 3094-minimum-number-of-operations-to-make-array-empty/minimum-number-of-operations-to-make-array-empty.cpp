67757b398ea18bf1ca78714b90b911e7127c0e8f
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-01-05 00:01:34 +0530

    Time: 120 ms (61.88%) | Memory: 81.9 MB (95.95%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>ans;
+        int count=1;
+        int a=0;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                count++;
+                
+            }
+            else{
+                ans.push_back(count);
+                count=1;
+            }
+            
+        }
+
+        ans.push_back(count);
+        cout<<ans[0]<<" ";
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]==1){
+                return -1;
+            }
+            if(ans[i]%3==0){
+                a+=ans[i]/3;
+            }
+            if(ans[i]%3==2){
+                a+=ans[i]/3 +1;
+            }
+            if(ans[i]%3==1&&ans[i]%2==0){
+                a+=ans[i]/3+1;
+            }
+            if(ans[i]%3==1&&ans[i]%2==1){
+                a+=ans[i]/3+1;
+            }
+        }
+        
+        return a;
+    }
+};
