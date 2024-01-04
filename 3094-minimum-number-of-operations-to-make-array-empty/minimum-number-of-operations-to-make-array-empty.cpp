f3088d4936cd04b476e2997ae5c37932f4916b88
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-01-05 00:02:38 +0530

    Time: 120 ms (61.88%) | Memory: 81.9 MB (95.95%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         vector<int>ans;
         int count=1;
-        int a=0;
+        
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i]==nums[i+1]){
                 count++;
@@ -18,25 +18,26 @@ public:
         }
 
         ans.push_back(count);
-        cout<<ans[0]<<" ";
+        count=0;
+        
         for(int i=0;i<ans.size();i++){
             if(ans[i]==1){
                 return -1;
             }
             if(ans[i]%3==0){
-                a+=ans[i]/3;
+                count+=ans[i]/3;
             }
             if(ans[i]%3==2){
-                a+=ans[i]/3 +1;
+                count+=ans[i]/3 +1;
             }
             if(ans[i]%3==1&&ans[i]%2==0){
-                a+=ans[i]/3+1;
+                count+=ans[i]/3+1;
             }
             if(ans[i]%3==1&&ans[i]%2==1){
-                a+=ans[i]/3+1;
+                count+=ans[i]/3+1;
             }
         }
         
-        return a;
+        return count;
     }
 };
