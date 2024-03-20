bf6aec257817d29ec9dafed0647bd22a343cd9e8
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-03-20 17:21:24 +0530

    Time: 176 ms (68.26%) | Memory: 98 MB (82.89%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+        int count=0;
+        ListNode* head=list1;
+        while(count!=a-1){
+            head=head->next;
+            count++;
+        }
+        ListNode* temp=head;
+        head=list1;
+        count=0;
+         while(count!=b){
+            head=head->next;
+            count++;
+        }
+       ListNode* temp2=head;
+        
+        
+        ListNode* temporary=list2;
+        
+        while(temporary->next!=NULL){
+            temporary=temporary->next;
+        }
+        temp->next=list2;
+        temporary->next=temp2->next;
+        temp2->next=NULL;
+        return list1;
+
+        
+    }
+};
