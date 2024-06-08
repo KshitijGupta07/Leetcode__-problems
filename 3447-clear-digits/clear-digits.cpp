5c9bd21ed197c8cfcf87a40fcb7ab4cb68788230
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-06-08 22:48:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (50.00%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -3,11 +3,13 @@ public:
     string clearDigits(string s) {
         string ans="";
         for(int i=0;i<s.length();i++){
+            
             if(s[i]>='a'&&s[i]<='z'){
                 ans.push_back(s[i]);
             }
             else{
-                if((ans[ans.length()-1]>='a'&&ans[ans.length()-1]<='z')&&(s[i]>='0'&&s[i]<='9')){
+                int q=ans.length()-1;
+                if((ans[q]>='a'&&ans[q]<='z')&&(s[i]>='0'&&s[i]<='9')){
                     ans.pop_back();
                 }
             }
