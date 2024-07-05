79b5b3685c7571085d7010254e967e3bc68e27e6
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-07-05 19:48:09 +0530

    Time: 44 ms (65.19%) | Memory: 54.7 MB (39.84%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.cpp
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
-        long long  sum=0;
-        long long  count=0;
+        int count1=0;
+        int count2=0;
+        int ans1=0;
+
+        
         for(int i=0;i<nums.size();i++){
-              if(nums[i]<=right){
-                count++;
-              }
-              else{
-                
-                sum=sum+(count*count+count)/2;
-                count=0;
-              }
-        }
-        long long  count2=0;
-        long long  sum2=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<left){
-                count2++;
+            if(nums[i]>=left&&nums[i]<=right){
+                count1++;
+                ans1+=count1;
+                count2=0;
             }
             else{
-                sum2+=(count2*(count2+1))/2;
-                count2=0;
+                if(nums[i]<left){
+                    count1++;
+                    ans1+=count1;
+                    count2++;
+                    ans1-=count2;
+                    
+                }
+                else{
+                   count1=0;
+                
+                   count2=0;
+                   
+                }
             }
         }
         
-        return int(sum+(count*(count+1))/2-(sum2+(count2*(count2+1))/2));
+        return ans1;
     }
 };
