39e1df31f51272be6b8461647f3828c5c16bf262
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-07-06 10:32:55 +0530

    Time: 2 ms (54.14%) | Memory: 7.1 MB (30.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        if(time>n){
-            int a=time/(n-1);
-            time=time-a*(n-1);
-            cout<<time<<endl;
-            if(a%2==0){
-                return time +1;
-            }
-            else{
-                return n-time;
-            }
+         if(time<n){
+            return time+1;
+         }
+        int a=n-1;
+        if((time/a)%2==0){
+            return time%a +1;
         }
-        if(time==n){
-            return n-1;
-        }
-        return time +1;
-        
+        return n- time%a;
+         
     }
 };
