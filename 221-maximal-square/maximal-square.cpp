20d8352daca1f242954246ed7593f545d9fdb12d
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-07-19 17:35:24 +0530

    Time: 49 ms (97.04%) | Memory: 27.2 MB (7.25%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -23,10 +23,30 @@ public:
        
          
     }
+    int solvetab(vector<vector<char>>&matrix,int &maxi){
+        vector<vector<int>>dp(matrix.size()+1,vector<int>(matrix[0].size()+1,0));
+    
+       for(int i=matrix.size()-1;i>=0;i--){
+        for(int j=matrix[0].size()-1;j>=0;j--){
+            int right=dp[i][j+1];
+            int diagonal=dp[i+1][j+1];
+            int bottom=dp[i+1][j];
+            if(matrix[i][j]=='1'){
+            int ans=1+min(right,min(diagonal,bottom));
+            dp[i][j]=ans;
+            maxi=max(maxi,ans);
+            }
+            else{
+                dp[i][j]=0;
+            }
+        }
+       }
+       return dp[0][0];
+    }
     int maximalSquare(vector<vector<char>>& matrix) {
         int maxi=0;
        vector<vector<int>>dp(matrix.size(),vector<int>(matrix[0].size(),-1));
-        int ans=solve(matrix,0,0,maxi,dp);
+        int ans=solvetab(matrix,maxi);
         return maxi*maxi;
     }
 };
