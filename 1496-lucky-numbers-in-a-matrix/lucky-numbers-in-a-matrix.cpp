bd8ef55716fed871d8ea42ce2fb3f8473a24c9bc
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-07-19 13:26:52 +0530

    Time: 21 ms (14.23%) | Memory: 14.3 MB (18.75%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    pair<int,int> findmin(vector<int>&v){
+        int mini=INT_MAX;
+        int miniindex=0;
+        for(int i=0;i<v.size();i++){
+           if(mini>v[i]){
+            mini=v[i];
+            miniindex=i;
+           }
+        }
+        return {mini,miniindex};
+    }
+    
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        vector<int>ans;
+        int j=0;
+        int count=0;
+         for(int i=0;i<matrix[0].size();i++){
+            int maxi=INT_MIN;
+            int maxindex=0;
+            for( j=0;j<matrix.size();j++){
+               if(matrix[j][i]>maxi){
+                maxi=matrix[j][i];
+                maxindex=j;
+               }
+            }
+            
+                if(maxi==findmin(matrix[maxindex]).first){
+                    if(matrix[maxindex][i]==maxi){
+                        ans.push_back(maxi);
+                    }
+                
+            
+            }
+            
+
+         }
+         for(int i=0;i<matrix.size();i++){
+            cout<<findmin(matrix[i]).first<<" ";
+         }
+         return ans;
+    }
+};
