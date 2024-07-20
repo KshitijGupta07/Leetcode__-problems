6d493d3d9b342cf1cff8bff6d2692219af9af32c
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-07-20 12:58:20 +0530

    Time: 519 ms (15.48%) | Memory: 294.8 MB (9.34%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int prev,int curr,vector<vector<int>>&dp){
-        
-         if(curr>=nums.size()){
-            return 0;
-         }
-          if(dp[curr][prev+1]!=-1){
-            return dp[curr][prev+1];
-          }
-         int include=0;
-         if(prev==-1||nums[curr]>nums[prev])
-          include=1+solve(nums,curr,curr+1,dp);
-           int exclude=solve(nums,prev,curr+1,dp);
-         dp[curr][prev+1]= max(include,exclude);
-         return dp[curr][prev+1];
-         
-    }
+   int solve(vector<int>&nums,int curr,int prev,vector<vector<int>>&dp){
+      if(curr>=nums.size()){
+        return 0;
+      }
+      if(dp[curr][prev+1]!=-1){
+        return dp[curr][prev+1];
+      }
+      int exclude=solve(nums,curr+1,prev,dp);
+      int include=0;
+      if(prev==-1||nums[curr]>nums[prev]){
+        include=1+solve(nums,curr+1,curr,dp);
+      }
+      
+      dp[curr][prev+1]= max(include,exclude);
+      
+      return dp[curr][prev+1];
+
+   }
+   int solvetab(vector<int>&nums){
+     vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,0));
+      for(int i=nums.size()-1;i>=0;i--){
+        for(int j=i-1;j>=-1;j--){
+            int include=0;
+            if(j==-1||nums[i]>nums[j]){
+                include=1+dp[i+1][i];
+            }
+            int exclude=dp[i+1][j];
+            dp[i][j]=max(include,exclude);
+
+        }
+      }
+      return dp[0][0];
+   }
     int lengthOfLIS(vector<int>& nums) {
         vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
-        
-       return  solve(nums,-1,0,dp);
+        int ans=solve(nums,0,-1,dp);
+
+        return ans;
     }
 };
