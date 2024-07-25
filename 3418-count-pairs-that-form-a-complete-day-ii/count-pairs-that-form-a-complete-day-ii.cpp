5cefd28c588b1001030b0ffc3d00c2d38a1e0832
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-07-25 17:03:04 +0530

    Time: 171 ms (35.86%) | Memory: 139.6 MB (9.82%) - LeetSync

--- 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.cpp
+++ 3418-count-pairs-that-form-a-complete-day-ii/count-pairs-that-form-a-complete-day-ii.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     long long countCompleteDayPairs(vector<int>& hours) {
-         unordered_map<int,int>m;
-         long long ans=0;
+        long long ans=0;
+        map<long long ,long long>m;
         for(int i=0;i<hours.size();i++){
-          
             ans+=m[(24-hours[i]%24)%24];
-              m[hours[i]%24]++;
+            m[hours[i]%24]++;
         }
         return ans;
-    
     }
 };
