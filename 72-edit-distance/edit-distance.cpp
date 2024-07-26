2ff0ff084add3c2d90014bcfddf026f4151d909b
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-07-26 15:28:29 +0530

    Time: 58 ms (9.79%) | Memory: 85.4 MB (5.30%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -24,11 +24,39 @@ public:
         dp[i][j]=ans;
         return dp[i][j];
     }
+    int solvetab(string word1,string word2){
+        vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1,0));
+          for(int i=0;i<word2.length();i++){
+            dp[word1.length()][i]=word2.length()-i;
+          }
+          for(int j=0;j<word1.length();j++){
+            dp[j][word2.length()]=word1.length()-j;
+          }
+          for(int i=word1.length()-1;i>=0;i--){
+            for(int j=word2.length()-1;j>=0;j--){
+                int ans=0;
+                if(word1[i]==word2[j]){
+                    ans=dp[i+1][j+1];
+                }
+                else{
+                    int insert=1+dp[i][j+1];
+                    int erase=1+dp[i+1][j];
+                    int replace=1+dp[i+1][j+1];
+                    ans=min(insert,min(erase,replace));
+                    
+                }
+                dp[i][j]=ans;
+                
+            }
+          }
+          return dp[0][0];
+        
+    }
     int minDistance(string word1, string word2) {
         int i=0;
         int j=0;
         vector<vector<int>>dp(word1.length(),vector<int>(word2.length(),-1));
         int ans=solve(word1,word2,i,j,dp);
-        return ans;
+        return solvetab(word1,word2);
     }
 };
