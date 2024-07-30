095ee36943780bc4e454108afb7506ed6520ced3
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-07-30 15:16:29 +0530

    Time: 91 ms (60.29%) | Memory: 23.9 MB (64.75%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int count=0;
-        stack<char>st;
+        string t="";
         for(int i=0;i<s.length();i++){
-            if(st.empty()==true){
-                st.push(s[i]);
+            if(t.empty()==true){
+                t.push_back(s[i]);
             }
             else{
-                if(st.empty()==false){
-                    if(s[i]=='a'&&st.top()=='b'){
-                        st.pop();
+                if(t.empty()==false){
+                    if(s[i]=='a'&&t[t.length()-1]=='b'){
+                        t.pop_back();
                         
                         count++;
                     }
                     else{
-                        st.push(s[i]);
+                        t.push_back(s[i]);
                     }
                 }
             }
