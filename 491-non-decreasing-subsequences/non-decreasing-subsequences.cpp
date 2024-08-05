a6d984f198e7b6046a37fc0858a6fbfdde11e421
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-08-05 18:46:05 +0530

    Time: 217 ms (9.89%) | Memory: 63.8 MB (18.19%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>&ans,vector<int>&temp,int prev,int curr,vector<int>&nums,map<int,bool>&m){
-        if(curr>=nums.size()){
-            if(temp.size()>1){
-            ans.push_back(temp);
-            }
-            return;
-        }
-       if(prev==-1||(nums[curr]>=nums[prev]&&m[curr]==false)){
+    // void solve(vector<vector<int>>&ans,vector<int>&temp,int prev,int curr,vector<int>&nums,map<int,bool>&m){
+    //     if(curr>=nums.size()){
+    //         if(temp.size()>1){
+    //         ans.push_back(temp);
+    //         }
+    //         return;
+    //     }
+    //    if(prev==-1||(nums[curr]>=nums[prev]&&m[curr]==false)){
 
-          temp.push_back(nums[curr]);
+    //       temp.push_back(nums[curr]);
           
           
-          solve(ans,temp,curr,curr+1,nums,m);
+    //       solve(ans,temp,curr,curr+1,nums,m);
         
-        temp.pop_back();
+    //     temp.pop_back();
           
-       } 
-        if(prev==-1||nums[curr]!=temp.back()){
-       solve(ans,temp,prev,curr+1,nums,m);
-        }
-    }
+    //    } 
+    //     if(prev==-1||nums[curr]!=temp.back()){
+    //    solve(ans,temp,prev,curr+1,nums,m);
+    //     }
+    // }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<vector<int>>ans;
+        set<vector<int>>ans;
         vector<int>temp;
         map<int,bool>m;
-        solve(ans,temp,-1,0,nums,m);
-        return ans;
+
+        for(int i=0;i<(1<<nums.size());i++){
+            vector<int>temp;
+            for(int j=0;j<nums.size();j++){
+                if((1&(i>>j))==1){
+                    if(temp.empty()==true||temp.back()<=nums[j]){
+                         temp.push_back(nums[j]);
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+                  if(temp.size()>1){
+            ans.insert(temp);
+                  }
+        }
+       
+        return vector (ans.begin(),ans.end());
     }
 };
