7e0bd7b3e5f80e39a1177ca989029b3042dc8ca0
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-09-10 13:29:03 +0530

    Time: 98 ms (54.02%) | Memory: 127.6 MB (7.27%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
-        int sum = 0;
-        for (int i = 0; i < rolls.size(); i++) {
-            sum = sum + rolls[i];
-        }
-        // Find the remaining sum.
-        int remainingSum = mean * (n + rolls.size()) - sum;
-        // Check if sum is valid or not.
-        if (remainingSum > 6 * n or remainingSum < n) {
+        int totalsum=(n+rolls.size())*mean;
+       int sum=0;
+       for(auto j:rolls){
+        sum+=j;
+       }
+       if(totalsum-sum>6*n||totalsum-sum<0){
+        return {};
+       }
+       vector<int>ans;
+       for(int i=0;i<n;i++){
+        ans.push_back((totalsum-sum)/n);
+       }
+       if((totalsum-sum)%n==0){
+        return ans;
+       }
+       int a=(totalsum-sum)%n;
+       for(int i=0;i<a;i++){
+        ans[i]+=1;
+       }
+       for(auto j:ans){
+        if(j>6||j<=0){
             return {};
         }
-        int distributeMean = remainingSum / n;
-        int mod = remainingSum % n;
-        // Distribute the remaining mod elements in nElements array.
-        vector<int> nElements(n, distributeMean);
-        for (int i = 0; i < mod; i++) {
-            nElements[i]++;
-        }
-        return nElements;
+       }
+       return ans;
+
     }
 };
