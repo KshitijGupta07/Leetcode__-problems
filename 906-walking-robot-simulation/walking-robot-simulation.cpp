d9c127e88e527ef05c36dfde952f3b73af643931
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-09-10 13:05:51 +0530

    Time: 188 ms (5.02%) | Memory: 69.4 MB (5.25%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -1,24 +1,102 @@
 class Solution {
 public:
-     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        unordered_set<string> obs;
-        for(int i=0;i<obstacles.size();i++) obs.insert(to_string(obstacles[i][0])+"#"+to_string(obstacles[i][1]));
-        int res=0, dir=0, x=0, y=0;
-        vector<vector<int>> ds={{0,1}, {1,0}, {0,-1}, {-1,0}};
-        for(int i=0;i<commands.size();i++) {
-            if(commands[i]==-2) dir--;
-            else if(commands[i]==-1) dir++;
-            else {
-                for(int j=0;j<commands[i];j++) {
-                    string pos=to_string(x+ds[dir][0])+"#"+to_string(y+ds[dir][1]);
-                    if(obs.find(pos)!=obs.end()) break;
-                    x+=ds[dir][0], y+=ds[dir][1];
+   int helper(vector<int>&commands,int& i,int& x,int &y,char& ch,map<vector<int>,bool>&location,bool& visited){
+           int maxi=INT_MIN;
+            if(ch=='N'){
+                for(int j=1;j<=commands[i];j++){
+                     y=y+1;
+                    if(location[{x,y}]){
+                        y=y-1;
+                    }
+                    
+                    maxi=max(maxi,x*x+y*y);
                 }
-                res=max(res, x*x+y*y);
             }
-            if(dir==-1) dir=3;
-            if(dir==4) dir=0;
+              if(ch=='S'){
+                for(int j=1;j<=commands[i];j++){
+                    y=y-1;
+                    if(location[{x,y}]){
+                        y=y+1;
+                    }
+                    
+                     maxi=max(maxi,x*x+y*y);
+                }
+            }
+              if(ch=='E'){
+                for(int j=1;j<=commands[i];j++){
+                    x=x+1;
+                    if(location[{x,y}]){
+                        x=x-1;
+                    }
+                    
+                     maxi=max(maxi,x*x+y*y);
+                }
+            }
+              if(ch=='W'){
+                for(int j=1;j<=commands[i];j++){
+                    x=x-1;
+                    if(location[{x,y}]){
+                        x=x+1;
+                        
+                    }
+                    
+                     maxi=max(maxi,x*x+y*y);
+                }
+            }
+              
+          return maxi;
+   }
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        map<vector<int>,bool>location;
+        for(int i=0;i<obstacles.size();i++){
+            location[obstacles[i]]=true;
         }
-        return res;
+        bool visited=false;
+        bool s=false;
+        int x=0;
+        int y=0;
+        char ch='N';
+         int maxi=INT_MIN;
+        for(int i=0;i<commands.size();i++){
+    
+            if(commands[i]==-1){
+                if(ch=='N'){
+                    ch='E';
+                }
+                else if(ch=='W'){
+                    ch='N';
+                }
+                else if(ch=='S'){
+                    ch='W';
+                }
+                else{
+                    ch='S';
+                }
+            }
+            else if(commands[i]==-2){
+                   if(ch=='N'){
+                    ch='W';
+                }
+                else if(ch=='W'){
+                    ch='S';
+                }
+                else if(ch=='S'){
+                    ch='E';
+                }
+                else{
+                    ch='N';
+                }
+            }
+           
+            else{
+                int element=helper(commands,i,x,y,ch,location,visited);
+               
+                maxi=max(maxi,element);
+            }
+        }
+        cout<<x<<" "<<y<<endl;
+    cout<<maxi<<endl;
+        
+        return maxi;
     }
 };
