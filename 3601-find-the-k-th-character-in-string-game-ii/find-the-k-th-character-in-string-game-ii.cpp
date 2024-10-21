f51d0bf8dbe05a5dfa7af04b13e216af95a953f5
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-10-21 14:54:04 +0530

    Time: 15 ms (5.52%) | Memory: 27.8 MB (42.77%) - LeetSync

--- 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.cpp
+++ 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.cpp
@@ -1,50 +1,50 @@
 class Solution {
 public:
-int found(long long k){
+  int found(long long k){
     for(int i=0;i<100;i++){
-        long long element=pow(2,i+1);
+        long long element=pow(2,i);
         if(element>k){
+             return i-1;
+        }
+        else if(element==k){
             return i;
         }
-        if(element==k){
-          return i+1;
-        }
+        
     }
     return -1;
-}
+  }
   char solve(long long k,vector<int>&operations){
-      if(k==1){
-        if(operations[0]==1){
-            return 'b';
-        }
-        else{
+         if(k==1){
+            if(operations[0]==1){
+                return 'b';
+            }
+            else{
+                return 'a';
+            }
+         }
+         if(k==0){
             return 'a';
-        }
+         }
 
-      }
-      if(k==0){
-        return 'a';
-      }
-      int index=found(k);
-      cout<<index<<endl;
-      long long  a=k-pow(2,index);
-      
-      if(operations[index]==1){
-      int l=(solve(a,operations)+1)-'a';
-      l=l%26;
-      return 'a'+l;
-       
-      }
+        
+        
+         int index=found(k);
+         cout<<index<<" ";
+         long long pos=k-pow(2,index);
+         if(operations[index]==1){
+            int ans=solve(pos,operations)-'a'+1;
+            ans=ans%26;
+            return 'a'+(ans)%26;
+         }
+         int ans=solve(pos,operations)-'a';
+         ans=ans%26;
+         return 'a'+ans;
 
-        int l=( solve(a,operations))-'a';
-        l=l%26;
-        return 'a'+l;
-      
-      
 
   }
     char kthCharacter(long long k, vector<int>& operations) {
+    
         char ans=solve(k-1,operations);
-        return ans;
+             return ans;
     }
 };
