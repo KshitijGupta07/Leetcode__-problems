d392f383b107728c7564dbd62bfbae997da8edac
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-10-21 10:44:13 +0530

    Time: 86 ms (98.71%) | Memory: 87.5 MB (5.04%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-
     int minSubarray(vector<int>& nums, int p) {
-        int  sum=0;
+        vector<int>prefix;
+        int element=0;
         for(int i=0;i<nums.size();i++){
-            sum=(sum+nums[i])%p;
+            element+=nums[i];
+            element=element%p;
         }
-        int  a=sum%p;
-        
-        if(a==0){
+        element=element%p;
+        if(element==0){
             return 0;
         }
-        int c=0;
-        unordered_map<int,int>m;
-        int l=nums.size();
-        m[0]=-1;
+        
+        unordered_map<int,int>visited;
+        visited[0]=-1;
+        int length=nums.size();
+        int sum=0;
         for(int i=0;i<nums.size();i++){
-              c=(c+nums[i])%p;
-             int n=(c-a+p)%p;
-           if( m.find(n)!=m.end()){
-                l=min(l,i-m[n]);
-           }
-           m[c]=i;
+            sum=(sum+nums[i])%p;
+            int need=(sum-element+p)%p;
+            if(visited.find(need)!=visited.end()){
+                length=min(length,i-visited[need]);
+            }
+            visited[sum]=i;
         }
-        cout<<l<<endl;
-        if(l==nums.size()){
+        if(length==nums.size()){
             return -1;
         }
-        return l;
-        
-        
-      
+        return length;
     }
 };
