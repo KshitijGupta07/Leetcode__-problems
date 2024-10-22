8005a1984c15c231a3bbbabc6ef68fcfbfd23026
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-10-22 15:09:34 +0530

    Time: 2 ms (83.78%) | Memory: 16.3 MB (58.38%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int count=0;
         int count1=0;
-        int count2=0;
-        int count=-1;
+        int ans=0;
         queue<pair<int,int>>q;
         for(int i=0;i<grid.size();i++){
-            for(int k=0;k<grid[0].size();k++){
-                if(grid[i][k]==2){
-                    q.push({i,k});
-                    count2++;
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                    count++;
                 }
-                if(grid[i][k]==1){
+                else if(grid[i][j]==1){
                     count1++;
                 }
             }
@@ -19,36 +19,45 @@ public:
         if(count1==0){
             return 0;
         }
-        if(count2==0){
+        if(count==0){
             return -1;
         }
-        vector<int>a{1,0,-1,0};
-        vector<int>b{0,1,0,-1};
-        while(q.empty()==false){
-            int size=q.size();
-            while(size--){
-            pair<int,int>front=q.front();
-            q.pop();
-            int x=front.first;
-            int y=front.second;
-            for(int i=0;i<4;i++){
-               int newx=x+a[i];
-               int newy=y+b[i];
-               if(newx>=0&&newx<grid.size()&&newy>=0&&newy<grid[0].size()&&grid[newx][newy]==1){
-                grid[newx][newy]=2;
-                count1--;
-                q.push({newx,newy});
-               }
-            }
-            }
-            count++;
+          vector<int>a{1,-1,0,0};
+          vector<int>b{0,0,1,-1};
+          while(q.empty()==false){
+              int size=q.size();
+              bool s=false;
+              while(size!=0){
+                 pair<int,int>front=q.front();
+             
+              q.pop();
+                size--;
+              
+              int x=front.first;
+              int y=front.second;
+            
+              for(int i=0;i<4;i++){
+                int newx=x+a[i];
+                int newy=y+b[i];
+                if(newx>=0&&newx<grid.size()&&newy>=0&&newy<grid[0].size()&&grid[newx][newy]==1){
+                    grid[newx][newy]=2;
+                    count1--;
+                    s=true;
+                    q.push({newx,newy});
+                }
+               
+              }
+        
+              }
 
-        } 
-        cout<<count1<<endl;
-        if(count1==0){
-        return count;
-        }
-        return -1;
-
+               if(s==true){
+              ans=ans+1;   
+               } 
+                
+          }
+          if(count1==0){
+            return ans;
+          }
+          return -1;
     }
 };
