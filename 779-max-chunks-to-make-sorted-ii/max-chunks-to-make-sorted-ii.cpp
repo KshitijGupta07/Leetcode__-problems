3bc32a36e4d1094f54d4793856b398c03c9d7a1f
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2024-12-19 16:38:03 +0530

    Time: 99 ms (8.13%) | Memory: 19.9 MB (8.37%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
@@ -3,16 +3,15 @@ public:
     int maxChunksToSorted(vector<int>& arr) {
         vector<int>compare=arr;
         sort(compare.begin(),compare.end());
-        vector<int>compare2;
-        vector<int>compare3;
+        map<int,int>m1;
+        map<int,int>m2;
         int n=arr.size();
         int count=0;
         for(int i=0;i<n;i++){
-            compare2.push_back(arr[i]);
-            compare3.push_back(compare[i]);
-            sort(compare2.begin(),compare2.end());
-            sort(compare3.begin(),compare3.end());
-            if(compare2==compare3){
+            m1[arr[i]]++;
+            m2[compare[i]]++;
+           
+            if(m1==m2){
                 count++;
             }
         }
