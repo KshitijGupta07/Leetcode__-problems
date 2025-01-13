23c6c2092f46e5a31d655d540eed79a78f8c7f7d
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-01-13 18:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (50.68%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,57 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
-        vector<int>ans;
-        if(s[0]=='}'||s[0]==')'||s[0]==']'){
-            return false;
-        }
-        if(s=="(])"){
-            return false;
-        }
-        if(s=="([}}])"){
-            return false;
-        }
-        if(s=="[](])"){
-            return false;
-        }
-
-        int count=0;
+        stack<char>st;
         for(int i=0;i<s.length();i++){
-            
-           if(s[i]=='('){
-                ans.push_back(s[i]);
+            if(st.empty()==true){
+                st.push(s[i]);
             }
-            if(s[i]=='{'){
-                ans.push_back(s[i]);
-            }
-            if(s[i]=='['){
-                ans.push_back(s[i]);
-            }
-        
-            if(ans.size()==0&&(s[i]=='}'||s[i]==')'||s[i]==']')){
-                return false;
-            }
-            if(s[i]==')'&&ans[ans.size()-1]=='('){
-               ans.pop_back();
-            }
-             if(s[i]=='}'&&ans[ans.size()-1]=='{'){
-                 ans.pop_back();
-            }
-            if(s[i]==']'&&ans[ans.size()-1]=='['){
-                
-                ans.pop_back();
+            else{
+                if(st.top()=='['&&s[i]==']'){
+                    st.pop();
+                }
+              else  if(st.top()=='('&&s[i]==')'){
+                    st.pop();
+                }
+               else if(st.top()=='{'&&s[i]=='}'){
+                    st.pop();
+                }
+                else{
+                    st.push(s[i]);
+                }
+
                 
             }
-            
-            
-            
-            
-            
-            
-            
         }
-            
-        if(ans.empty()==true){
+        if(st.empty()){
             return true;
         }
         return false;
