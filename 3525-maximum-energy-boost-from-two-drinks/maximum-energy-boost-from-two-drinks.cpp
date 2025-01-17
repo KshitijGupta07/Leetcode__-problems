b2da908b4f71c58b0ca711071e7ba420eff2c8ba
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-01-17 15:34:19 +0530

    Time: 201 ms (15.90%) | Memory: 268.1 MB (24.47%) - LeetSync

--- 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.cpp
+++ 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-  long long solve(vector<int>& energyDrinkA, vector<int>& energyDrinkB,int index,int curr,vector<vector<long long>>&dp){
-    if(index>=energyDrinkA.size()){
-        return 0;
-    }
-    long long ans=0;
-    if(dp[index][curr]!=-1){
-        return dp[index][curr];
-    }
-    if(curr==1){
-           ans=max(ans,energyDrinkA[index]+solve(energyDrinkA,energyDrinkB,index+1,1,dp));
-    } 
-    else{
-        ans=max(ans,energyDrinkB[index]+solve(energyDrinkA,energyDrinkB,index+1,0,dp));
-    }
-    ans=max(ans,solve(energyDrinkA,energyDrinkB,index+1,1-curr,dp));
-    dp[index][curr]=ans;
-    return dp[index][curr];
-    
-  }
-    long long maxEnergyBoost(vector<int>& energyDrinkA, vector<int>& energyDrinkB) {
-        long long ans=0;
-        int n=energyDrinkA.size();
-        int index=0;
-        vector<vector<long long>>dp(n,vector<long long>(2,-1));
-        ans=max(solve(energyDrinkA,energyDrinkB,0,0,dp),solve(energyDrinkA,energyDrinkB,0,1,dp));
-        return ans;
+    long long solve(vector<int>&A,vector<int>&B,int i,int c,vector<vector<long long >>&dp){
+        if(i>=A.size()){
+            return 0;
+        }
+        if(dp[i][c]!=-1){
+            return dp[i][c];
+        }
+        long long a=0;
+        
+        if(c==0)
+        a=A[i]+max(solve(A,B,i+1,c,dp),solve(A,B,i+2,1-c,dp));
+        else{
+            a=B[i]+max(solve(A,B,i+1,c,dp),solve(A,B,i+2,1-c,dp));
+        }
+        return dp[i][c]=a;
 
+
+    }
+    long long maxEnergyBoost(vector<int>& A, vector<int>& B) {
+        int i=0;
+        vector<vector<long long >>dp(A.size(),vector<long long >(2,-1));
+        long long ans=max(solve(A,B,i,0,dp),solve(A,B,i,1,dp));
+        return ans;
     }
 };
