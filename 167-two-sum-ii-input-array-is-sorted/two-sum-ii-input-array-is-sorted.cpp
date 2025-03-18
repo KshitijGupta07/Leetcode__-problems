14f50628ff2b170345359d60bda77eee206c9d97
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-03-18 18:37:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (93.98%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-   int binary_search(vector<int>& numbers,int target){
-       int start=0;
-       int end=numbers.size()-1;
-       while(start<=end){
-           int mid=start+(end-start)/2;
-           if(numbers[mid]==target){
-               return mid;
-               break;
-           }
-           else if(numbers[mid]>target){
-               end=mid-1;
-
-           }
-           else{
-               start=mid+1;
-           }
-       }
-       return -1;
-   }
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int>ans;
-        for(int i=0;i<numbers.size();i++){
-            if(binary_search(numbers,target-numbers[i])!=-1){
-                ans.push_back(i+1);
-                ans.push_back(binary_search(numbers,target-numbers[i])+1);
-                break;
+        int start=0;
+        int end=numbers.size()-1;
+        while(start<end){
+            if(numbers[start]+numbers[end]==target){
+                return {start+1,end+1};
+            }
+            else if(numbers[start]+numbers[end]>target){
+                end--;
+            }
+            else{
+                start++;
             }
         }
-        if(ans[0]==ans[1]){
-            ans[1]=ans[1]+1;
-        }
-        return ans;
-        
+        return {-1,-1};
     }
 };
