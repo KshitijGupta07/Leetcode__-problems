caac80ebc978a0de5d1d7dbb2757101715b72bae
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-03-22 00:43:11 +0530

    Time: 3 ms (3.90%) | Memory: 9 MB (8.85%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return double(pow(x,n));
+        if(n==0){
+            return 1.000;
+        }
+        
+        double ans=1.00;
+        vector<int>num;
+        
+        while(n!=0){
+            if(n%2==0){
+                num.push_back(n);
+                n=n/2;
+            }
+            else{
+                num.push_back(n);
+                if(n>0){
+                    n-=1;
+                }
+                else{
+                    n+=1;
+                }
+            }
+
+        }
+        reverse(num.begin(),num.end());
+        for(auto j:num){
+            cout<<ans<<" "<<j<<endl;
+            if(j%2==1||j%2==-1){
+                if(j>0){
+                    ans*=x;
+                }
+                else{
+                    ans/=x;
+                }
+            }
+            else{
+                ans*=ans;
+            }
+        }
+        return ans;
     }
 };
