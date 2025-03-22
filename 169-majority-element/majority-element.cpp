f8f9d4ec4872e6dc4e2228140ec1823968e428cc
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-03-22 17:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (99.86%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,16 +4,16 @@ public:
         int element=0;
         int count=0;
         for(int i=0;i<nums.size();i++){
-            if(count==0){
+              if(count==0){
                 element=nums[i];
                 count=1;
-            }
-            else if(element==nums[i]){
+              }
+              else if(element==nums[i]){
                 count++;
-            }
-            else{
+              }
+              else{
                 count--;
-            }
+              }
         }
         return element;
     }
