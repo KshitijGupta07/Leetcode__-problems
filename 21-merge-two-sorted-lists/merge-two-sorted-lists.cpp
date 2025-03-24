3793bf1381806daf90d54f974619de47002b1512
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-03-24 10:17:31 +0530

    Time: 19 ms (1.21%) | Memory: 19.6 MB (28.41%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -8,65 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- 
 class Solution {
 public:
-void insertattail(ListNode* &tail,int &val){
-     ListNode* newnode=new ListNode(val);
-     if(tail==NULL){
-        tail=newnode;
-     }
-     else{
-         tail->next=newnode;
-         tail=newnode;
-     }
- }
- 
- 
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
         if(list1==NULL){
             return list2;
         }
         if(list2==NULL){
             return list1;
         }
-        ListNode* tail=NULL;
-        ListNode* head1=list1;
-        ListNode* head2=list2;
         ListNode* head=NULL;
-        int count=0;
-        while(head1!=NULL&&head2!=NULL){
-            
-            
-            if(head1->val>head2->val){
-                insertattail(tail,head2->val);
-                head2=head2->next;
-            }
-            else{
-                insertattail(tail,head1->val);
-                head1=head1->next;
-            }
-            count++;
-            if(count==1){
-                head=tail;
-            }
-            
-            
-            
+        ListNode* temp=NULL;
+        while(list1!=NULL&&list2!=NULL){
+            cout<<list1->val<<" "<<list2->val<<endl;
+             if(list1->val>=list2->val){
+                if(head==NULL){
+                    head=new ListNode(list2->val);
+                    temp=head;
+                    list2=list2->next;
+                }
+                else{
+                    temp->next=new ListNode(list2->val);
+                    temp=temp->next;
+                    list2=list2->next;
+                }
+             }
+             else{
+                if(head==NULL){
+                    head=new ListNode(list1->val);
+                     temp=head;
+                    list1=list1->next;
+                }
+                else{
+                     temp->next=new ListNode(list1->val);
+                     temp=temp->next;
+                     list1=list1->next;
+                }
+             }
         }
-        if(head1!=NULL){
-            while(head1!=NULL){
-             insertattail(tail,head1->val);
-             head1=head1->next;
-            }
-
+        while(list1!=NULL){
+            temp->next=new ListNode(list1->val);
+            temp=temp->next;
+            list1=list1->next;
         }
-        if(head2!=NULL){
-            while(head2!=NULL){
-                insertattail(tail,head2->val);
-                head2=head2->next;
-            }
+           while(list2!=NULL){
+            temp->next=new ListNode(list2->val);
+            temp=temp->next;
+            list2=list2->next;
         }
         return head;
     }
