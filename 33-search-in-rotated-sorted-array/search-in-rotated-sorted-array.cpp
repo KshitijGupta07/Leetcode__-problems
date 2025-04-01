8097edeb9bcf00655fda7da0b0ca1f9e06464dea
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-04-01 19:27:56 +0530

    Time: 1 ms (4.21%) | Memory: 15.3 MB (33.69%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,105 +1,58 @@
 class Solution {
 public:
-    int pivotelement(vector<int>&nums){
+    int pivot(vector<int>&nums){
         int start=0;
         int end=nums.size()-1;
-        while(start<end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]>nums[0]){
-                start=mid+1;
+        while(start<=end){
+            int mid=(start+end)/2 ;
+            if(mid<nums.size()-1&&nums[mid]<nums[mid+1]){
+                if(nums[mid]>nums[0]){
+                    start=mid+1;
+                }
+                else if(mid>0&&nums[mid]<nums[mid-1]){
+                     return mid-1;
+                }
+                else{
+                    end=mid-1;
+                }
             }
             else{
-                end=mid;
+                return mid;
             }
-            
-        }
-        return end;
-    }
-    int search(vector<int>& nums, int target) {
-        if(nums[pivotelement(nums)]==target){
-            return pivotelement(nums);
-        }
-        if(nums.size()==1){
-            if(nums[0]==target){
-                return 0;
-            }
-            else{
-                return -1;
-            }
-        }
-        if(nums.size()==2){
-            if(nums[0]==target){
-                return 0;
-            }
-           else if(nums[1]==target){
-                return 1;
-            }
-            else{
-                return -1;
-            }
+
         }
         
-        if(pivotelement(nums)>0&&pivotelement(nums)<nums.size()){
-        if(target>=nums[0]){
-          int start=0;
-          int end=pivotelement(nums)-1;
-          while(start<=end){
-              int mid=start+(end-start)/2;
-              if(nums[mid]==target){
-                  return mid;
-              }
-              else if(nums[mid]>target){
-                    end=mid-1;
-              }
-              else{
-                  start=mid+1;
-              }
-          }
-          
+        return start;
+    }
+     int binarysearch(vector<int>&nums,int start,int end,int target){
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else{
+                start=mid +1;
+            }
         }
-    else{
-         int start=pivotelement(nums);
-          int end=nums.size()-1;
-          while(start<=end){
-              int mid=start+(end-start)/2;
-              if(nums[mid]==target){
-                  return mid;
-              }
-              else if(nums[mid]>target){
-                    end=mid-1;
-              }
-              else{
-                  start=mid+1;
-              }
-          }
-          
+        return -1;
+     }
+    int search(vector<int>& nums, int target) {
+        int p= pivot(nums);
+        int start=-1;
+        int end=-1;
+        cout<<p<<endl;
+        if(target>=nums[0]&&target<=nums[p]){
+              start=0;
+              end=p;
         }
-    }
-    
-    cout<<pivotelement(nums)<<endl;
-    if(pivotelement(nums)==0||pivotelement(nums)==nums.size()-1){
-        int start=0;
-          int end=nums.size()-1;
-          while(start<=end){
-              int mid=start+(end-start)/2;
-              if(nums[mid]==target){
-                  return mid;
-              }
-              else if(nums[mid]>target){
-                    end=mid-1;
-              }
-              else {
-                  start=mid+1;
-              }
-          }
-         
-          
-    }
-    
-    if(pivotelement(nums)==0&&nums[0]>nums[1]&&nums[1]==target){
-        return 1;
-    }
-    
-    return -1;
+        else{
+            start=p+1;
+            end=nums.size()-1;
+        }
+        int ans=binarysearch(nums,start,end,target);
+        return ans;
     }
 };
