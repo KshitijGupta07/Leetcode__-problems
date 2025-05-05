81546323f7dfa4c85b0d13a48beef87705fcb682
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-05-05 12:36:53 +0530

    Time: 505 ms (12.17%) | Memory: 8.9 MB (29.08%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,47 +1,63 @@
 class Solution {
 public:
- bool issafe(vector<vector<char>>& board,int row,int col,int val){
-    for(int i=0;i<board.size();i++){
-        if(board[row][i]==val+'0'){
-            return false;
+   bool check(int i,int j,char ch,vector<vector<char>>&board){
+        for(int x=0;x<board.size();x++){
+              if(board[i][x]==ch&&x!=j){
+                return false;
+              }
         }
-        if(board[i][col]==val+'0'){
-            return false;
+        for(int x=0;x<board.size();x++){
+            if(board[x][j]==ch&&x!=i){
+                return false;
+            }
         }
-        if(board[3*(row/3)+i/3][3*(col/3)+i%3]==val+'0'){
-            return false;
-        }
-    }
-    return true;
- }
-  bool solve(vector<vector<char>>& board){
-    for(int i=0;i<board.size();i++){
-        for(int j=0;j<board[0].size();j++){
-
-            //traversing element
-            if(board[i][j]=='.'){
-                //if we see empty place 
-                for(int val=1;val<=9;val++){
-                    //chceking value
-                    if(issafe(board,i,j,val)){
-                        board[i][j]=val+'0';
-                        bool solution=solve(board);
-                        if(solution){
-                            return true;
-                        }
-                        else{
-                        board[i][j]='.';
-                        }
+        int mod=i%3;
+        int mod2=j%3;
+        int row1=i-mod;
+        int row2=i+(2-mod);
+        int col1=j-mod2;
+        int col2=j+(2-mod2);
+        for(int x=row1;x<=row2;x++){
+            for(int y=col1;y<=col2;y++){
+                if(board[x][y]==ch){
+                    if(x==i&&y==j){
+                        continue;
+                    }
+                    else{
+                        return false;
                     }
                 }
-               return false;
             }
-            
         }
+        return true;
+
+   }
+    bool  solve(vector<vector<char>>&board){
+         for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                 if(board[i][j]=='.'){
+                    for(int x=1;x<=9;x++){
+                        
+                        if(check(i,j,x+'0',board)){
+                            board[i][j]=x+'0';
+                            if(solve(board)){
+                                return true;
+                            }
+                            else{
+                            board[i][j]='.';
+                            }
+                        }
+                    
+                            
+
+                    }
+                    return false;
+                 }
+            }
+          }
+          return true;
     }
-    return true;
-  }
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+         solve(board);
     }
 };
