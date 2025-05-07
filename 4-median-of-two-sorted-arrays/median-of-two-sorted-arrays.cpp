2f35bffbbeeb1fbe9b6d3ebf13c14c93891bd4e4
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-05-07 23:53:54 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (80.08%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,61 +1,42 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()==0){
-            if(nums2.size()%2==0){
-                return (double(nums2[nums2.size()/2])+double(nums2[nums2.size()/2 -1]))/2;
+         double x=0;
+         double y=0;
+         int i=0;
+         int j=0;
+         int m=nums1.size();
+         int n=nums2.size();
+        int count=(m+n)/2 ;
+        while(count>=0){
+            y=x;
+            if(i<m&&j<n){
+                if(nums1[i]>nums2[j]){
+                    x=nums2[j];
+                    j++;
+                }
+                else{
+                    x=nums1[i];
+                    i++;
+                }
             }
-            else{
-               return (double(nums2[nums2.size()/2 ])); 
-            }
-        }
-        if(nums2.size()==0){
-             if(nums1.size()%2==0){
-                return (double(nums1[nums1.size()/2])+double(nums1[nums1.size()/2 -1]))/2;
-            }
-            else{
-               return (double(nums1[nums1.size()/2 ])); 
-            }
-        }
-        vector<int>ans;
-        int i=0;
-        int j=0;
-        while(i<nums1.size()&&j<nums2.size()){
-            if(nums1[i]>nums2[j]){
-                ans.push_back(nums2[j]);
-                j++;
-            }
-            else{
-                ans.push_back(nums1[i]);
+            else if(i<m){
+                x=nums1[i];
                 i++;
             }
-        }
-        cout<<i<<" "<<j<<endl;
-        if(i!=nums1.size()-1){
-            for(int k=i;k<=nums1.size()-1;k++){
-                ans.push_back(nums1[k]);
+            else{
+                x=nums2[j];
+                j++;
             }
-        }
-        if(j!=nums2.size()-1){
-            for(int k=j;k<=nums2.size()-1;k++){
-                ans.push_back(nums2[k]);
-            }
-        }
-        if(i==nums1.size()-1){
-            ans.push_back(nums1[nums1.size()-1]);
-        }
-        if(j==nums2.size()-1){
-            ans.push_back(nums2[nums2.size()-1]);
-        }
-        if(ans.size()%2==0){
-                   return (double(ans[ans.size()/2])+double(ans[ans.size()/2 -1]))/2;
             
-            
+            count--;
         }
-        else{
-            return double(ans[ans.size()/2]);
+        count=(m+n) ;
+        if(count%2==1){
+            return max(x,y)/1.000 ;
         }
-        return -1;
         
+    
+        return (x+y)/2.000;
     }
 };
