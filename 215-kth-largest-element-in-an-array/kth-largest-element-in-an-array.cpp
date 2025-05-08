ecbc4bde235e2794414a94f3acc311d34ab58934
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-05-08 15:25:03 +0530

    Time: 29 ms (63.26%) | Memory: 59.2 MB (83.10%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,69 +1,32 @@
 class Solution {
 public:
-    void insert(int val,vector<int>&arr,int size){
-        size=size+1;
-        int index=size;
-        arr[index]=val;
-        while(index>1){
-            int parent=index/2;
-            if(arr[parent]>arr[index]){
-                swap(arr[parent],arr[index]);
-                index=parent;
-            }
-            else{
-                return;
-            }
+    void heapify(vector<int>&nums,int i,int n){
+        int large=i;
+        int left=2*i +1;
+        int right=2*i +2;
+        if(left<n&&nums[left]>nums[large]){
+            large=left;
         }
+        if(right<n&&nums[right]>nums[large]){
+            large=right;
+        }
+        if(large!=i){
+              swap(nums[large],nums[i]);
+              heapify(nums,large,n);
+        }
+     
+
     }
-    
-       
     int findKthLargest(vector<int>& nums, int k) {
-        vector<int>arr(k+1);
-        
-        arr[0]=-1;
-        int size=1;
-        for(int i=1;i<=k;i++){
-            insert(nums[i-1],arr,i-1);
-    
-        }
-         for(int i=k;i<nums.size();i++){
-              if(nums[i]>=arr[1]){
-                arr[1]=nums[i];
-                int j=1;
-                int large=j;
-                 while(large<arr.size()){
-            int leftindex=2*large;
-            int rightindex=2*large+1;
-            if(leftindex<arr.size()&&arr[leftindex]<arr[large]){
-                
-                large=leftindex;
-            }
-             if(rightindex<arr.size()&&arr[rightindex]<arr[large]){
-                
-                large=rightindex;
-            }
-            if(large!=j){
-                swap(arr[large],arr[j]);
-                j=large;
-            }
-            else{
-                break;
-            }
-        }
-              }
-
-            }
-        
-        
-                
-        for(int i=0;i<arr.size();i++){
-            cout<<arr[i]<<" ";
-        }
-        cout<<endl;
-        
+        int n=nums.size();
+       for(int i=n/2 -1;i>=0;i--){
+           heapify(nums,i,n);
+       }
+       for(int i=n-1;i>n-k;i--){
+           swap(nums[0],nums[i]);
+           heapify(nums,0,i);
+       }
        
-        
-        return arr[1];
-
+       return nums[0];
     }
 };
