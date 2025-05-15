19f233d39c3f0c4790d686bb4c303567d31ac323
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-05-15 20:10:48 +0530

    Time: 414 ms (14.91%) | Memory: 292.1 MB (9.74%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,51 +1,40 @@
 class Solution {
 public:
-   bool reverse(string& s,int start ,int end,vector<vector<int>>&dp){
-    if(start<s.length()&&end>=0&&start>=end){
-        return true;
-    }
-    bool ans=false;
-    if(dp[start][end]!=-1){
-        return dp[start][end];
-    }
-    if(s[start]==s[end]){
-       ans= reverse(s,start+1,end-1,dp);
-    }
-    else{
-        ans=false;
-    }
-     dp[start][end]=ans;
-     return dp[start][end];
-   }
-   string solve(string &s){
-    vector<vector<int>>dp(s.length(),vector<int>(s.length(),-1));
-    int start=0;
-    int maxend=INT_MIN;
-    int maxlen=INT_MIN;
-    for(int i=0;i<s.length();i++){
-        for(int j=i;j<s.length();j++){
-        
-            if(reverse(s,i,j,dp)==true){
-                if(maxlen<j-i+1){
-                    maxlen=j-i+1;
-                    start=i;
-                    maxend=j;
-                }
-                  
-            }
-        }
-    }
-    string ans="";
-     for(int i=start;i<=maxend;i++){
-        ans.push_back(s[i]);
-     }
-    return ans;
-      
+   bool check(string& s,int i,int j,vector<vector<int>>&dp){
+       if(i<s.length()&&j>=0&&i>=j){
+        return dp[i][j]=true;
+       }
+       if(dp[i][j]!=-1){
+        return dp[i][j];
+       }
+       if(s[i]!=s[j]){
+        return dp[i][j]=false;
+       }
+       
+       return dp[i][j]=check(s,i+1,j-1,dp);
    }
     string longestPalindrome(string s) {
-        int start=0;
-        int end=s.length()-1;
-        string ans=solve(s);
+        int start=-1;
+        int end=-1;
+        int maxlen=INT_MIN;
+        vector<vector<int>>dp(s.length(),vector<int>(s.length(),-1));
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                if(check(s,i,j,dp)){
+                   
+                     if(maxlen<j-i+1){
+                         start=i;
+                         end=j;
+                        maxlen=j-i+1;
+                     }
+                }
+            }
+        }
+        string ans="";
+        for(int i=start;i<=end;i++){
+            ans.push_back(s[i]);
+        }
+        cout<<start<<" "<<end<<endl;
         return ans;
     }
 };
