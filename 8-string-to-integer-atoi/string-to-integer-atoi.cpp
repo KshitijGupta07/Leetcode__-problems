dcaf4b8f649e8346949fdb4736d9dc3ce2a3d454
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-05-16 01:58:24 +0530

    Time: 5 ms (4.41%) | Memory: 9.9 MB (6.25%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,72 +1,80 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int counter=0;
-        int counter1=0;
-        string number="";
-        for(int i=0;i<s.length();i++){
-            if(s[i]==' '){
-                if(number.length()>0){
-                    break;
-                }
-                continue;
-            }
-            else if(s[i]=='-'){
-                if(counter==0&&counter1==0){
-                counter++;
-                number.push_back(s[i]);
-                }
-                else{
-                    break;
-                }
-            }
-            else if(s[i]=='+'){
-                if(counter1==0&&counter==0){
-                counter1++;
-                number.push_back(s[i]);
-                }
-                else{
-                    break;
-                }
-            }
-            else if(s[i]>='0'&&s[i]<='9'){
-                if(counter1==0){
-                    counter1++;
-                }
-                number.push_back(s[i]);
+        int n=s.length();
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(s[i]!=' '&&isdigit(s[i])==false&&s[i]!='-'&&s[i]!='+'){
+                break;
+                
             }
             else{
-                break;
-            }
-        }
-        if(number.length()==0){
-            return 0;
-        }
-      
-        if(number.length()==1){
-            if(number[0]=='-'){
-                return 0;
-            }
-            if(number[0]=='+'){
-                return 0;
-            }
-        }
-            
-            string x="";
-            for(int i=0;i<number.length();i++){
-                x.push_back(number[i]);
-                if(x.length()>1){
-                    long long n=stoll(x);
-                    if(n>INT_MAX){
-                        return INT_MAX;
+                if(isdigit(s[i])){
+                ans.push_back(s[i]);
+                }
+                if(s[i]=='-'){
+                    
+                    if(ans.length()>0){
+                        break;
                     }
-                    if(n<INT_MIN){
-                        return INT_MIN;
+                    else{
+                        ans.push_back(s[i]);
+                    }
+                }
+                 if(s[i]=='+'){
+                    if(ans.length()>0){
+                        break;
+                    }
+                    else{
+                        ans.push_back(s[i]);
+                    }
+                }
+               
+                if(s[i]=='0'){
+                    if(ans.length()==0){
+                        continue;
+                    }
+                }
+                if(s[i]==' '){
+                    if(ans.length()>0){
+                        break;
                     }
                 }
             }
-            return stoi(number);
-        
 
+        }
+        
+        string x="";
+    
+        for(int i=0;i<ans.length();i++){
+            
+            if(isdigit(ans[i])){
+                x.push_back(ans[i]);
+            }
+            if(x.length()>0&&stoll(x)>INT_MAX){
+                if(ans[0]=='-'){
+                    return INT_MIN;
+                }
+                else{
+                    return INT_MAX;
+                }
+            }
+            
+        }
+        cout<<x<<endl;
+        if(ans.length()==0){
+            return 0;
+        }
+        if(ans.length()==1){
+            if(ans[0]=='+'||ans[0]==
+            '-'){
+                return 0;
+            }
+        }
+        if(ans[0]=='-'){
+            return -stoi(x);
+        }
+
+        return stoi(x);
     }
 };
