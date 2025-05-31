0af6cd7bea4cd78a9e91c9bbaeb84787f8d3a2b1
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-05-31 12:36:41 +0530

    Time: 10 ms (5.71%) | Memory: 16.6 MB (12.69%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,34 +11,48 @@
  */
 class Solution {
 public:
+   void dfs(TreeNode* &root,map<pair<int,int>,vector<int>>&m,int num,int level){
+       if(root==NULL){
+        return;
+       }
+       m[{level,num}].push_back(root->val);
+       level=level+1;
+       dfs(root->left,m,num-1,level);
+       dfs(root->right,m,num+1,level);
+   }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> m;
-        queue<pair<TreeNode*,pair<int,int>>> q;
-        q.push({root,{0,0}});
-        while(!q.empty()){
-            auto node=q.front();
-            q.pop();
-            root=node.first;
-            int x=node.second.first;
-            int y=node.second.second;
-            m[x][y].insert(root->val);
-            if(root->left)
-            {
-                q.push({root->left,{x-1,y+1}});
-            }
-            if(root->right)
-            {
-                q.push({root->right,{x+1,y+1}});
-            }
+        map<pair<int,int>,vector<int>>m;
+        int level=0;
+        dfs(root,m,0,level);
+      
+        map<int,vector<int>>mp;
+       vector<vector<int>>ans;
+       for(auto j:m){
+    
+        vector<int>temp=j.second;
+        sort(temp.begin(),temp.end());
+        m[j.first]=temp;
+        
+       }
+       for(auto j:m){
+         for(auto k:j.second){
+            cout<<k<<" ";
+         }
+         cout<<endl;
+       }
+       for(auto j:m){
+          
+        for(auto k:j.second){
+            
+             mp[j.first.second].push_back(k);
         }
-        vector<vector<int>> ans;
-        for(auto i:m){
-            vector<int> v;
-            for(auto j:i.second){
-                v.insert(v.end(),j.second.begin(),j.second.end());
-            }
-            ans.push_back(v);
-        }
-        return ans;
+        cout<<endl;
+       }
+       for(auto j:mp){
+        
+         ans.push_back(j.second);
+        
+       }
+       return ans;
     }
 };
