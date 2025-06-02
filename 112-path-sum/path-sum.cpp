b2efaaaa735eda29f185c64830520af16d10c89c
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-02 10:34:58 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (94.68%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -11,32 +11,30 @@
  */
 class Solution {
 public:
+    bool solve(TreeNode* root,int targetSum){
+        if(root==NULL){
+            return false;
+        }
+        if(root->left==NULL&&root->right==NULL){
+        
+            if(targetSum==root->val){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        targetSum-=root->val;
+        bool l=solve(root->left,targetSum);
+        bool r=solve(root->right,targetSum);
+        return l||r;
+    }
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root==NULL){
             return false;
         }
-        stack<TreeNode*>t;
-        stack<int>sum;
-        sum.push(root->val);
-        t.push(root);
-        while(t.empty()==false){
-            int tem=sum.top();
-            TreeNode* temp=t.top();
-            sum.pop();
-            t.pop();
-            if(temp->left==NULL&&temp->right==NULL&&tem==targetSum){
-                return true;
-            }
-             if(temp->left!=NULL){
-                 t.push(temp->left);
-                 sum.push(tem+temp->left->val);
-            }
-             if(temp->right!=NULL){
-                t.push(temp->right);
-                sum.push(tem+temp->right->val);
-            }
-            cout<<tem<<endl;
-        }
-        return false;
+        bool ans=solve(root,targetSum);
+        return ans;
+        
     }
 };
