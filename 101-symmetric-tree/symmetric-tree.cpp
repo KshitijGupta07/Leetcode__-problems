be4aea441c0cc3fe88c32952872e0953752c2765
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-02 09:51:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (61.32%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,21 +11,21 @@
  */
 class Solution {
 public:
-    
     bool mirror(TreeNode* l,TreeNode* r){
-        if(l==NULL&&r==NULL){
-            return true;
-        }
         if(l!=NULL&&r==NULL){
             return false;
         }
         if(l==NULL&&r!=NULL){
             return false;
         }
-        return(l->val==r->val)&&mirror(l->left,r->right)&&mirror(l->right,r->left);
+        if(l==NULL&&r==NULL){
+            return true;
+        }
+        return (l->val==r->val)&&mirror(l->left,r->right)&&mirror(l->right,r->left);
     }
     bool isSymmetric(TreeNode* root) {
-        return mirror(root->left,root->right);
-       
+        
+        bool ans=mirror(root->left,root->right);
+        return ans;
     }
 };
