94a3850c211c6fc163bf1bca2ca37fd2b8b6d753
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-03 15:53:13 +0530

    Time: 594 ms (5.14%) | Memory: 37.3 MB (60.57%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -11,29 +11,42 @@
  */
 class Solution {
 public:
-      void inorder(TreeNode* root,vector<int>&ans){
+    bool search(TreeNode* root,int k){
         if(root==NULL){
-          return ;
+            return false;
         }
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-      }
+        while(root!=NULL){
+            cout<<root->val<<" "<<k<<endl;
+            if(root->val==k){
+                return true;
+            }
+            else if(root->val>k){
+                root=root->left;
+            }
+            else{
+                root=root->right;
+            }
+        }
+        return false;
+
+    }
+     void traverse(vector<int>&temp,TreeNode*root){
+          if(root==NULL){
+            return;
+          }
+          traverse(temp,root->left);
+          temp.push_back(root->val);
+           traverse(temp,root->right);
+     }
     bool findTarget(TreeNode* root, int k) {
-        
-       vector<int>ans;
-       inorder(root,ans); 
-       int start=0;
-       int end=ans.size()-1;
-       while(start<end){
-        if(ans[start]+ans[end]==k){
+        if(root->left==NULL&&root->right==NULL){
+            return false;
+        }
+       vector<int>temp;
+       traverse(temp,root);
+       for(auto j:temp){
+        if(k-j!=j&&search(root,k-j)){
             return true;
-        }
-        else if(ans[start]+ans[end]>k){
-            end--;
-        }
-        else{
-            start++;
         }
        }
        return false;
