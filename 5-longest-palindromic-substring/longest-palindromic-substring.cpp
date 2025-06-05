4fdbf563b03bce4fa7b90c538f4ee7f3fa6cdfb2
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-05 16:53:37 +0530

    Time: 495 ms (14.33%) | Memory: 292 MB (9.97%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-   bool check(string& s,int i,int j,vector<vector<int>>&dp){
+   bool check(string &s,int i,int j,vector<vector<int>>&dp){
        if(i<s.length()&&j>=0&&i>=j){
         return dp[i][j]=true;
        }
-       if(dp[i][j]!=-1){
-        return dp[i][j];
-       }
        if(s[i]!=s[j]){
         return dp[i][j]=false;
+       }
+       if(dp[i][j]!=-1){
+        return dp[i][j];
        }
        
        return dp[i][j]=check(s,i+1,j-1,dp);
    }
     string longestPalindrome(string s) {
-        int start=-1;
-        int end=-1;
-        int maxlen=INT_MIN;
+        int ans=0;
+        int start=0;
+        int end=0;
         vector<vector<int>>dp(s.length(),vector<int>(s.length(),-1));
         for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                if(check(s,i,j,dp)){
-                   
-                     if(maxlen<j-i+1){
-                         start=i;
-                         end=j;
-                        maxlen=j-i+1;
+            for(int j=0;j<s.length();j++){
+                  if(check(s,i,j,dp)){
+                     if(ans<j-i+1){
+                        start=i;
+                        end=j;
+                        ans=j-i+1;
                      }
-                }
+                  }
             }
         }
-        string ans="";
-        for(int i=start;i<=end;i++){
-            ans.push_back(s[i]);
+        string pali="";
+        for(int x=start;x<=end;x++){
+             pali.push_back(s[x]);
         }
-        cout<<start<<" "<<end<<endl;
-        return ans;
+        return pali;
     }
 };
