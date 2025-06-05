f3d57c7c149c10580bcd76c5e36f5c9bd2ac8fc6
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-05 11:51:44 +0530

    Time: 31 ms (35.94%) | Memory: 45.3 MB (6.26%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
-    int solvetab(string text1,string text2){
-        vector<vector<int>>dp(text1.length()+1,vector<int>(text2.length()+1,0));
-    
-        for(int i=text1.length()-1;i>=0;i--){
-            int ans=0;
-            for(int j=text2.length()-1;j>=0;j--){
-                if(text1[i]==text2[j]){
-                    ans=1+dp[i+1][j+1];
-                }
-                else{
-                    ans=max(dp[i+1][j],dp[i][j+1]);
-                }
-                  dp[i][j]=ans;
+   int solve(string text1,string text2,int i,int j,vector<vector<int>>&dp){
+      if(i>=text1.length()||j>=text2.length()){
+        return 0;
+      }
+      if(dp[i][j]!=-1){
+        return dp[i][j];
+      }
+      int ans=0;
+      if(text1[i]==text2[j]){
+         ans=1+solve(text1,text2,i+1,j+1,dp);
+      }
+      else{
+        ans=max(ans,solve(text1,text2,i+1,j,dp));
+        ans=max(ans,solve(text1,text2,i,j+1,dp));
+      }
+      return dp[i][j]=ans;
+   }
+   int tab(string text1,string text2){
+    vector<vector<int>>dp(text1.length()+1,vector<int>(text2.length()+1,0));
+    for(int i=text1.length()-1;i>=0;i--){
+        int ans=0;
+        for(int j=text2.length()-1;j>=0;j--){
+            if(text1[i]==text2[j]){
+                ans=1+dp[i+1][j+1];
             }
-          
+            else{
+                ans=max(ans,max(dp[i+1][j],dp[i][j+1]));
+            }
+            dp[i][j]=ans;
         }
-        return dp[0][0];
-
     }
+    return dp[0][0];
+   }
     int longestCommonSubsequence(string text1, string text2) {
         int i=0;
         int j=0;
         vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
-        int ans=solvetab(text1,text2);
+        int ans=tab(text1,text2);
         return ans;
     }
 };
