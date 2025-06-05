7f39dbbfbae6ca157af82e69d0981e6c1ca110eb
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-05 12:19:44 +0530

    Time: 66 ms (9.81%) | Memory: 83.4 MB (5.64%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,62 +1,32 @@
 class Solution {
 public:
     int solve(string word1,string word2,int i,int j,vector<vector<int>>&dp){
-        if(i==word1.length()){
+        if(i>=word1.length()){
             return word2.length()-j;
         }
-        if(j==word2.length()){
+        if(j>=word2.length()){
             return word1.length()-i;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int ans=0;
+        int ans=INT_MAX;
         if(word1[i]==word2[j]){
-            ans=solve(word1,word2,i+1,j+1,dp);
+            ans=min(ans,solve(word1,word2,i+1,j+1,dp));
         }
         else{
-            int insert=1+solve(word1,word2,i,j+1,dp);
-            int erase=1+solve(word1,word2,i+1,j,dp);
-            int  replace=1+solve(word1,word2,i+1,j+1,dp);
-            ans=min(insert,min(erase,replace));
-            
+            ans=min(ans,1+solve(word1,word2,i+1,j,dp));
+            ans=min(ans,1+solve(word1,word2,i+1,j+1,dp));
+            ans=min(ans,1+solve(word1,word2,i,j+1,dp));
         }
-        dp[i][j]=ans;
-        return dp[i][j];
-    }
-    int solvetab(string word1,string word2){
-        vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1,0));
-          for(int i=0;i<word2.length();i++){
-            dp[word1.length()][i]=word2.length()-i;
-          }
-          for(int j=0;j<word1.length();j++){
-            dp[j][word2.length()]=word1.length()-j;
-          }
-          for(int i=word1.length()-1;i>=0;i--){
-            for(int j=word2.length()-1;j>=0;j--){
-                int ans=0;
-                if(word1[i]==word2[j]){
-                    ans=dp[i+1][j+1];
-                }
-                else{
-                    int insert=1+dp[i][j+1];
-                    int erase=1+dp[i+1][j];
-                    int replace=1+dp[i+1][j+1];
-                    ans=min(insert,min(erase,replace));
-                    
-                }
-                dp[i][j]=ans;
-                
-            }
-          }
-          return dp[0][0];
-        
+        return dp[i][j]=ans;
+
     }
     int minDistance(string word1, string word2) {
         int i=0;
         int j=0;
         vector<vector<int>>dp(word1.length(),vector<int>(word2.length(),-1));
         int ans=solve(word1,word2,i,j,dp);
-        return solvetab(word1,word2);
+        return ans;
     }
 };
