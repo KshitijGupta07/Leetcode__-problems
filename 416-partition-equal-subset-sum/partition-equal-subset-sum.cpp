7437553af0c1ec92fbf94fe38644f56c208a4795
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-06 11:34:48 +0530

    Time: 1178 ms (5.04%) | Memory: 402.6 MB (5.31%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,52 +1,32 @@
 class Solution {
 public:
-    bool solve(vector<int>&nums,int target,int index,vector<vector<int>>&dp){
-        if(index>=nums.size()){
-            return 0;
-        }
-        if(target<0){
-            return 0;
-        }
-        if(target==0){
-            return 1;
-        }
-        if(dp[target][index]!=-1){
-             return dp[target][index];
-        }
-        bool include=solve(nums,target-nums[index],index+1,dp);
-        bool exclude=solve(nums,target,index+1,dp);
-         dp[target][index]= include|exclude;
-         return dp[target][index];
+   bool solve(vector<int>&nums,int sum,int i,vector<vector<int>>&dp){
+    if(sum<0){
+        return false;
     }
-    bool solvetab(vector<int>&nums,int target){
-        vector<vector<int>>dp(target+1,vector<int>(nums.size()+1,0));
-           for(int i=0;i<nums.size()+1;i++){
-            dp[0][i]=1;
-           }
-           for(int i=target;i>=0;i--){
-            for(int j=nums.size()-1;j>=0;j--){
-                bool include=false;
-                if(i>=nums[j]){
-                 include=dp[i-nums[j]][j+1];
-                }
-                bool exclude=dp[i][j+1];
-                dp[i][j]=include|exclude;
-            }
-           }
-           return dp[target][0];
+    if(i>=nums.size()){
+        return sum==0;
     }
+    if(dp[sum][i]!=-1){
+        return dp[sum][i];
+    }
+    
+      bool inc=solve(nums,sum-nums[i],i+1,dp);
+      bool exc=solve(nums,sum,i+1,dp);
+      return dp[sum][i]=inc||exc;
+   }
     bool canPartition(vector<int>& nums) {
-        int totalsum=0;
         
-        for(int i=0;i<nums.size();i++){
-            totalsum+=nums[i];
+        int sum=0;
+        for(auto j:nums){
+            sum+=j;
         }
-        if(totalsum%2==1){
+        if(sum%2==1){
             return false;
         }
-        
-        int target=totalsum/2;
-        vector<vector<int>>dp(target+1,vector<int>(nums.size(),-1));
-        return solve(nums,target,0,dp);
+        int a=sum/2 ;
+        vector<vector<int>>dp(20001,vector<int>(201,-1));
+        bool ans=solve(nums,a,0,dp);
+        return ans;
     }
 };
