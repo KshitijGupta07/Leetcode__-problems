ee08899c11a3b93a0e6311a7a5c35b271880dec4
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-07 17:20:17 +0530

    Time: 19 ms (81.95%) | Memory: 22 MB (48.28%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
-   int solve(vector<int>&coins,int amount){
-    vector<int>dp(amount+1,INT_MAX);
+//    long long  solve(vector<int>&coins,int amount,vector<int>&dp){
+//       if(amount==0){
+//         return 0;
+//       }
+//       if(amount<0){
+//         return INT_MAX;
+//       }
+//       if(dp[amount]!=INT_MAX){
+//         return dp[amount];
+//       }
+//       long long ans=INT_MAX;
+//       for(int i=0;i<coins.size();i++){
+//              ans=min(ans,1+solve(coins,amount-coins[i],dp));
+//       }
+//       return dp[amount]=ans;
+
+//    }
+   int tab(vector<int>&coins,int amount){
+    vector<long long >dp(amount+1,INT_MAX);
     dp[0]=0;
-    
-    for(int i=1;i<=amount;i++){
-        int mini=INT_MAX;
-        for(int j=0;j<coins.size();j++){
-            if(i-coins[j]>-1){
-            int ans=dp[i-coins[j]];
-            if(ans!=INT_MAX)
-            dp[i]=min(dp[i],ans+1);
+    for(int i=0;i<coins.size();i++){
+        for(int j=1;j<=amount;j++){
+            if(j-coins[i]>=0){
+                if(dp[j-coins[i]]!=INT_MAX){
+                dp[j]=min(dp[j],1+dp[j-coins[i]]);
+                }
             }
+            
         }
         
-    
     }
     return dp[amount];
    }
-   
-   
     int coinChange(vector<int>& coins, int amount) {
-        
-        
-        
-        int ans=solve(coins,amount);
+        int index=0;
+    
+        int ans=tab(coins,amount);
         if(ans==INT_MAX){
             return -1;
         }
-        else{
-            return ans;
-        }
-        
-        
-        return -2;
+        return ans;
     }
 };
