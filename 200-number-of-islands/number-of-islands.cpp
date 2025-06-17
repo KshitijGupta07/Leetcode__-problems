5c48045a98f3d784cafb404af69ccd1fd9077051
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-17 16:23:54 +0530

    Time: 26 ms (64.81%) | Memory: 16.5 MB (65.24%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-
-   void solve(vector<vector<char>>&grid,int i,int j,vector<vector<bool>>&visited){
-       if(i<0||i>=grid.size()||j<0||j>=grid[0].size()||visited[i][j]==true||grid[i][j]=='0'){
-        return;
-       }
-       visited[i][j]=true;
-       solve(grid,i+1,j,visited);
-       solve(grid,i-1,j,visited);
-       solve(grid,i,j-1,visited);
-       solve(grid,i,j+1,visited);
+   void solve(vector<vector<char>>&grid,vector<vector<bool>>&visited,int i,int j,int m,int n){
+    if(i<0||j<0||i>=m||j>=n||visited[i][j]==true||grid[i][j]=='0'){
+        return ;
+    }
+     visited[i][j]=true;
+     solve(grid,visited,i,j+1,m,n);
+     solve(grid,visited,i,j-1,m,n);
+     solve(grid,visited,i+1,j,m,n);
+     solve(grid,visited,i-1,j,m,n);
    }
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size(),false));
-        int count=0;
-         for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<bool>>visited(m,vector<bool>(n,false));
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(visited[i][j]==false&&grid[i][j]=='1'){
-                       solve(grid,i,j,visited);
-                       count++;
+                    solve(grid,visited,i,j,m,n);
+                    ans++;
                 }
             }
-         }
-         return count;
+        }
+        return ans;
     }
 };
