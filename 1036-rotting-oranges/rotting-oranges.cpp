4ff462dd12e0035ea13cd61a82a28046e67fa2c3
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-06-17 16:41:07 +0530

    Time: 1 ms (41.67%) | Memory: 17.2 MB (34.26%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -2,62 +2,61 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<int,int>>q;
-        int counter=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size(),false));
+
+        int m=grid.size();
+        int n=grid[0].size();
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
+                    visited[i][j]=true;
                 }
                 if(grid[i][j]==1){
-                    counter++;
+                    count++;
                 }
             }
         }
-        if(counter==0){
+        if(count==0){
             return 0;
         }
-        vector<int>xi{1,-1,0,0};
-        vector<int>yi{0,0,-1,1};
-        int minute=0;
-        vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size(),false));
+        if(q.size()==0){
+            return -1;
+        }
+        vector<int>a{1,-1,0,0};
+        vector<int>b{0,0,1,-1};
+         int timer=0;
+
         while(q.empty()==false){
-                 int size=q.size();
-                 bool check=false;
-                 for(int j=0;j<size;j++){
-                    pair<int,int>front=q.front();
-                    int x=front.first;
-                    int y=front.second;
-                    q.pop();
-                    visited[x][y]=true;
-                    
-                     for(int i=0;i<4;i++){
-                        if(x+xi[i]<grid.size()&&y+yi[i]<grid[0].size()&&visited[x+xi[i]][y+yi[i]]==false){
-                            if(grid[x+xi[i]][y+yi[i]]==1){
-                                check=true;
-                                grid[x+xi[i]][y+yi[i]]=2;
-                                 q.push({x+xi[i],y+yi[i]});
-                            }
-                            if(grid[x+xi[i]][y+yi[i]]==2){
-                                q.push({x+xi[i],y+yi[i]});
-                            }
-                           
+               int size=q.size();
+               bool signal=false;
+               for(int i=0;i<size;i++){
+                
+                  int x=q.front().first;
+                  int y=q.front().second;
+                  q.pop();
+                  for(int i=0;i<4;i++){
+                     if(x+a[i]>=0&&x+a[i]<m&&y+b[i]>=0&&y+b[i]<n&&visited[x+a[i]][y+b[i]]==false){
+                        if(grid[x+a[i]][y+b[i]]==1){
+                            signal=true;
+                            count-=1;
+                            grid[x+a[i]][y+b[i]]=2;
+                            q.push({x+a[i],y+b[i]});
+                            visited[x+a[i]][y+b[i]]=true;
                         }
                      }
-                     
+                  }
 
-                 }
-                 if(check==true){
-                 minute++;
-                 }
+               }
+               if(signal){
+                timer++;
+               }
+               
         }
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-            }
-            cout<<endl;
+        if(count>0){
+            return -1;
         }
-        return minute ;
+        return timer;
     }
 };
