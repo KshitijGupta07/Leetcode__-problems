b92f9d80bd744c6dfbf8d9a8a98f3167fc6d61c7
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-25 16:21:29 +0530

    Time: 3 ms (83.51%) | Memory: 11.7 MB (68.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char,int>m;
+        vector<int>hash(256,0);
         int l=0;
         int r=0;
         int ans=0;
+        if(s.length()==0){
+            return 0;
+        }
+        if(s.length()==1){
+            return 1;
+        }
         while(r<s.length()){
-             m[s[r]]+=1;
-            while(m[s[r]]>1){
-                m[s[l]]-=1;
-                l+=1;
-                if(m[s[l]]==0){
-                    m.erase(s[l]);
+            if(hash[s[r]]==0){
+                hash[s[r]]=1;
+                r++;
+            }
+            else{
+                
+                if(hash[s[l]]==1){
+                    hash[s[l]]=0;
+                    l++;
+                    
+                    ans=max(ans,r-l+1);
                 }
             }
-           
-            ans=max(ans,r-l+1);
-            cout<<l<<" "<<r<<endl;
-            r+=1;
         }
-        return ans;
+        r=s.length()-1;
+        cout<<l<<" "<<r<<endl;
+        return max(ans,r-l+1);
     }
 };
