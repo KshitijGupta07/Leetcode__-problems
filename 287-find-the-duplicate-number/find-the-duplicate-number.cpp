1caedb517e43e32995a510ee740fe20c5b60fd75
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-25 10:50:50 +0530

    Time: 2 ms (72.18%) | Memory: 65.1 MB (69.74%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
-            int a=abs(nums[i])-1;
-    
-            nums[a]=-nums[a];
-            if(nums[a]>0){
-                return a+1 ;
-            }
-            
-        }
-        for(auto j:nums){
-            cout<<j<<endl;
+               int a=abs(nums[i])-1;
+               nums[a]=-nums[a];
+               if(nums[a]>0){
+                return a+1;
+               }
         }
         return -1;
     }
