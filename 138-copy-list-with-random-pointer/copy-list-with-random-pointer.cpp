480ff4b89108e0a350ee12be36cdad63119253c8
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-26 14:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (8.18%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,29 +17,36 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-         Node* node=head;
-         while(node){
-            Node* temp=node->next;
-            node->next=new Node(node->val);
-            node->next->next=temp;
-            node=temp;
-         }
-         node=head;
-         while(node){
-            if(node->random){
-                node->next->random=node->random->next;
+        if(head==NULL){
+            return NULL;
+        }
+        map<Node* ,Node*>m;
+        Node* temp=head;
+        Node* head2=NULL;
+        Node* temp2=NULL;
+        while(temp!=NULL){
+            m[temp]=new Node(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            if(head2==NULL){
+                head2=m[temp];
+                temp2=head2;
             }
-            node=node->next->next;
-         }
-         Node* ans=new Node(0);
-         Node* helper=ans;
-         while(head){
-               helper->next=head->next;
-               helper=helper->next;
-               head->next=head->next->next;
-               head=head->next;
-         }
-         return ans->next;
-
+            else{
+                temp2->next=m[temp];
+                temp2=temp2->next;
+            }
+            temp=temp->next;
+        }
+        temp=head;
+        temp2=head2;
+        while(temp!=NULL){
+              temp2->random=m[temp->random];
+              temp2=temp2->next;
+              temp=temp->next;
+        }
+        return head2;
     }
 };
