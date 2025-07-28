aa798bace6303c9d494fc8cc1392a7d5b45fd6fd
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-28 20:26:34 +0530

    Time: 46 ms (5.19%) | Memory: 20.7 MB (12.73%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,36 +1,56 @@
 class Solution {
 public:
-   bool solve(string s,map<string,bool>&m,int i,int j,vector<vector<int>>&dp){
-       if(j==s.length()-1){
-          return m[s.substr(i,j-i+1)];
-       }
-       if(dp[i][j]!=-1){
-        return dp[i][j];
-       }
-    
-       
-       if(m[s.substr(i,j-i+1)]){
-           bool ans1=solve(s,m,j+1,j+1,dp);
-           bool ans2=solve(s,m,i,j+1,dp);
-           return dp[i][j]=ans1||ans2;
+    bool solve(string s,int i,int j,map<string,bool>&found){
+        if(j==s.length()-1){
+            return found[s.substr(i,j-i+1)];
+        }
+        
+        
+        bool ans=false;
+        if(found[s.substr(i,j-i+1)]==true){
+            ans=solve(s,i,j+1,found)||solve(s,j+1,j+1,found);
 
-       }
-       
-        return dp[i][j]=solve(s,m,i,j+1,dp);
-       
-       
-       
-   }
+        }
+        else{
+            ans=solve(s,i,j+1,found);
+        }
+        return ans;
+    }
+    bool tab(string s,map<string,bool>&found){
+        vector<vector<bool>>dp(s.length()+1,vector<bool>(s.length()+1,false));
+        for(int i=0;i<s.length();i++){
+            dp[i][s.length()-1]=found[s.substr(i,s.length()-i)];
+        }
+        for(int i=s.length()-1;i>=0;i--){
+        
+            for(int j=s.length()-1;j>=i;j--){
+
+                if(found[s.substr(i,j-i+1)]){
+                    if(j+1<s.length()){
+                dp[i][j]=dp[i][j+1]||dp[j+1][j+1];
+                    }
+                    else{
+                        dp[i][j]=true;
+                    }
+                }
+                else{
+                    if(j+1<s.length()){
+                        dp[i][j]=dp[i][j+1];
+                    }
+                }
+            
+            }
+        }
+        return dp[0][0];
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        map<string,bool>m;
+        map<string,bool>found;
         for(auto j:wordDict){
-            m[j]=true;
+            found[j]=true;
         }
         int i=0;
-        string temp="";
-        m[""]=true;
-         vector<vector<int>>dp(s.length(),vector<int>(s.length(),-1));
-        bool ans=solve(s,m,i,0,dp);
+        int j=0;
+        bool ans=tab(s,found);
         return ans;
     }
 };
