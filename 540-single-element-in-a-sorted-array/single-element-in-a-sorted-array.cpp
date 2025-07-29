1689b2b940940efad69635f4055b956b7d1299a6
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-29 16:20:34 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (67.19%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,57 +1,46 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
-        }
         int start=0;
         int end=nums.size()-1;
-        while(start<=end){
-            int mid=(start+end)/2 ;
-            if(mid==0){
-                if(nums[mid]!=nums[mid+1]){
-                    return nums[mid];
-                }
-                else{
-                    start=mid +1;
-                }
-                
+        while(start<end){
+            int mid=(start+end)/2;
+            if(mid+1<nums.size()&&mid-1>=0&&nums[mid]!=nums[mid+1]&&nums[mid]!=nums[mid-1]){
+                return nums[mid];
             }
-            else if(mid==nums.size()-1){
-                if(nums[mid]!=nums[mid-1]){
-                    return nums[mid];
-                }
-                else{
-                    end=mid-1;
-                }
+            else if(mid==0&&nums[0]!=nums[1]){
+                return nums[0];
             }
-            
+            else if(mid==0&&nums[0]==nums[1]){
+                start=mid+1;
+            }
+            else if(mid==nums.size()-1&&nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+            else if(mid==nums.size()-1&&nums[mid]==nums[mid-1]){
+                end=mid-1;
+            }
             else{
-                if(nums[mid]!=nums[mid+1]&&nums[mid]!=nums[mid-1]){
-                    return nums[mid];
-                }
-                else{
-                    if(nums[mid-1]==nums[mid]){
-                         if(mid%2==0){
-                            end=mid-1;
-                         }
-                         else{
-                            start=mid +1;
-                         }
+                
+                if(mid-1>=0&&nums[mid]==nums[mid-1]){
+                    if(mid%2==0){
+                        end=mid-1;
                     }
-                    if(nums[mid]==nums[mid+1]){
-                        if(mid%2==0){
-                            start=mid +1;
-                        }
-                        else{
-                            end=mid-1;
-                        }
+                    else{
+                       start=mid+1; 
                     }
                 }
-
+                else if(mid+1<nums.size()&&nums[mid]==nums[mid+1]){
+                    if(mid%2==1){
+                        end=mid-1;
+                    }
+                    else{
+                        start=mid+1;
+                    }
+                }
+            }
         }
         
-    }
-    return -1;
+        return nums[start];
     }
 };
