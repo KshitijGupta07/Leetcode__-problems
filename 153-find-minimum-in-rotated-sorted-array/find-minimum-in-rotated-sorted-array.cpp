99a4cf4dc86ebf2e02987f1c01bd1ac2d3043462
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-29 15:48:52 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (97.47%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int start=0;
-        int end=nums.size()-1;
         if(nums.size()==1){
             return nums[0];
         }
-        int a=0;
+        int start=0;
+        int end=nums.size()-1;
         while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]>nums[0]){
-                if(mid+1<nums.size()&&nums[mid]>nums[mid+1]){
-                    a=mid;
-                    break;
-                }
-                else{
-                    start=mid+1;
-                }
+            int mid=(start+end)/2;
+            if(nums[0]<nums[mid]){
+                start=mid+1;
             }
             else{
-                if(mid-1>=0&&nums[mid]<nums[mid-1]){
-                    a=mid;
-                    break;
+                if(mid>0&&nums[mid]<nums[mid-1]){
+                    return nums[mid];
                 }
                 else{
                     end=mid-1;
                 }
             }
         }
-        cout<<a<<endl;
-        if(a==0){
-            return min(nums[0],nums[1]);
+        if(start==0){
+            return min(nums[start],nums[start+1]);
         }
-        if(a==nums.size()-1){
-            return min(nums[a],nums[0]);
+        if(start==nums.size()||start==nums.size()-1){
+            return nums[0];
         }
-        return min(nums[a],min(nums[a-1],nums[a+1]));
+        return min(nums[start],nums[start-1]);
+
     }
-             
-     
 };
