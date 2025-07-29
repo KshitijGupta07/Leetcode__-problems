d74825964c7f7424a013660d33d23ba1cf87cb58
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-29 12:25:08 +0530

    Time: 1 ms (2.68%) | Memory: 17.6 MB (17.51%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,63 +1,47 @@
 class Solution {
 public:
-int lastoccurence(vector<int>&nums,int target){
-    int start=0;
-    int end=nums.size()-1;
-     if(binary_search(nums.begin(),nums.end(),target)==true){
-    while(start<=end){
-        int mid=start+(end-start)/2;
-        if(nums[mid]>target){
-            end=mid-1;
-        }
-        else if(nums[mid]<target){
-            start=mid+1;
-        }
-        else{
-            if(mid==nums.size()-1||nums[mid]!=nums[mid+1]){
-                return mid;
-            }
-            else{
-                start=mid+1;
-            }
-        }
-        
-    }
-    return start;
-}
-    return -1;
-}
-int firstoccurence(vector<int>&nums,int target){
-    int start=0;
-    int end=nums.size()-1;
-    if(binary_search(nums.begin(),nums.end(),target)==true){
-    while(start<=end){
-        int mid=start+(end-start)/2;
-        if(nums[mid]>target){
-            end=mid-1;
-        }
-        else if(nums[mid]<target){
-            start=mid+1;
-        }
-        else{
-            if(mid==0||nums[mid]!=nums[mid-1]){
-                return mid;
-            }
-            else{
-                end=mid-1;
-            }
-        }
-    }
-    return start;
-}
-    return -1;
-}
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>ans;
-        int a=lastoccurence(nums,target);
-        int b=firstoccurence(nums,target);
+        int start=0;
+        int end=nums.size()-1;
+        int first=-1;
+        int last=-1;
+        while(start<=end){
 
-        ans.push_back(b);
-        ans.push_back(a);
-        return ans;
+            int mid=(start+end)/2;
+              
+              if(nums[mid]<target){
+                
+                start=mid+1;
+              }
+              else if(nums[mid]>target){
+                end=mid-1;
+              }
+              else{
+                 cout<<"hello1"<<endl;
+                first=mid;
+                end=mid-1;
+            
+              }
+        }
+        start=0;
+        end=nums.size()-1;
+        while(start<=end){
+             int mid=(start+end)/2;
+              
+              if(nums[mid]<target){
+                
+                start=mid+1;
+              }
+              else if(nums[mid]>target){
+                end=mid-1;
+              }
+              else{
+                 cout<<"hello1"<<endl;
+                last=mid;
+                start=mid+1;
+            
+              }
+        }
+        return {first,last};
     }
 };
