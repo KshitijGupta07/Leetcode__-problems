ed9d8833a8567ea53151d937168c8ec28d9fbcbb
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-30 01:19:03 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (79.11%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -9,34 +9,42 @@ public:
         int start=0;
         int end=m;
         int total=(m+n+1)/2;
-        
         while(start<=end){
-           
-            int mid=(start+end)/2 ;
-            int left=total-mid;
-             int l1=INT_MIN;
-        int l2=INT_MIN;
-        int r1=INT_MAX;
-        int r2=INT_MAX;
-            if(mid<m) r1=nums1[mid];
-            if(left<n)r2=nums2[left];
-            if(mid-1>=0)l1=nums1[mid-1];
-            if(left-1>=0)l2=nums2[left-1];
+            int mid=(start+end)/2;
+            int mid2=total-mid;
+            
+            int r1=INT_MAX;
+            
+            int r2=INT_MAX;
+            if(mid<m){
+                r1=nums1[mid];
+            }
+            if(mid2<n){
+                r2=nums2[mid2];
+            }
+            int l1=INT_MIN;
+            int l2=INT_MIN;
+            if(mid-1>=0){
+                 l1=nums1[mid-1];
+            }
+            if(mid2-1>=0){
+                l2=nums2[mid2-1];
+            }
             if(l1<=r2&&l2<=r1){
-                if((m+n)%2==1){
-                    return max(l1,l2)/1.00;
-                }
-                else{
+                if((m+n)%2==0){
                     l1=max(l1,l2);
                     r1=min(r1,r2);
-                    return (l1 +r1)/2.00;
+                    return ((l1+r1)/2.00);
+                }
+                else{
+                    return max(l1,l2)/1.00;
                 }
             }
             else if(l1>r2){
-                 end=mid-1;
+                   end=mid-1;
             }
             else{
-                start=mid +1;
+                start=mid+1;
             }
         }
         return 0.000;
