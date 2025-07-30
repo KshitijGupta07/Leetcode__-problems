eb10ffb611f17e7f8b1270a97b5a14ddbde30ef5
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-30 14:35:42 +0530

    Time: 28 ms (67.52%) | Memory: 59.2 MB (92.81%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     void heapify(vector<int>&nums,int i,int n){
-        int large=i;
-        int left=2*i +1;
-        int right=2*i +2;
-        if(left<n&&nums[left]>nums[large]){
-            large=left;
+        
+        int index=i;
+        if(2*i+1<n&&nums[2*i+1]>nums[index]){
+             index=2*i+1;
         }
-        if(right<n&&nums[right]>nums[large]){
-            large=right;
+        if(2*i+2 <n &&nums[2*i+2]>nums[index]){
+            index=2*i+2;
         }
-        if(large!=i){
-              swap(nums[large],nums[i]);
-              heapify(nums,large,n);
+        if(index!=i){
+            swap(nums[index],nums[i]);
+            heapify(nums,index,n);
         }
-     
-
     }
     int findKthLargest(vector<int>& nums, int k) {
         int n=nums.size();
-       for(int i=n/2 -1;i>=0;i--){
-           heapify(nums,i,n);
-       }
-       for(int i=n-1;i>n-k;i--){
-           swap(nums[0],nums[i]);
-           heapify(nums,0,i);
-       }
-       
-       return nums[0];
+        for(int i=n/2 -1;i>=0;i--){
+            heapify(nums,i,n);
+        }
+        for(int i=n-1;i>n-k;i--){
+            swap(nums[0],nums[i]);
+            heapify(nums,0,i);
+        }
+        return nums[0];
     }
 };
