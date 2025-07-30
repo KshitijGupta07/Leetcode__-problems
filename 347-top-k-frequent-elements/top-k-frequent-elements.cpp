0f09d99cfae1c95840721ea3e76d4e66d17ea8dd
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-07-30 16:16:13 +0530

    Time: 4 ms (34.86%) | Memory: 17.9 MB (55.26%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,48 +1,21 @@
 class Solution {
 public:
-   void heapify(vector<pair<int,int>>&v,int i,int n){
-     int large=i;
-     int left=2*i +1;
-     int right=2*i +2;
-     if(left<n&&v[left].first>v[large].first){
-        large=left;
-     }
-     if(right<n&&v[right].first>v[large].first){
-        large=right;
-     }
-     
-     if(large!=i){
-        swap(v[large],v[i]);
-        heapify(v,large,n);
-     }
-   }
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>m;
+        map<int,int>m;
         for(auto j:nums){
-            m[j]++;
+            m[j]+=1;
         }
-        vector<pair<int,int>>v;
-        
-        for(auto j:m){
-            v.push_back({j.second,j.first});
-        }
-        int n=v.size();
-        for(int i=n/2 -1;i>=0;i--){
-            heapify(v,i,v.size());
-        }
-       
         vector<int>ans;
-        ans.push_back(v[0].second);
-        for(int i=n-1;i>n-k;i--){
-              
-
-                 swap(v[0],v[i]);
-                  
-                 heapify(v,0,i);
-                 
-                  cout<<"next"<<endl;
-                 ans.push_back(v[0].second);
+        priority_queue<pair<int,int>>p;
+        for(auto j:m){
+            p.push({j.second,j.first});
+        }
+        while(k>0){
+            ans.push_back(p.top().second);
+            p.pop();
+            k-=1;
         }
         return ans;
+
     }
 };
