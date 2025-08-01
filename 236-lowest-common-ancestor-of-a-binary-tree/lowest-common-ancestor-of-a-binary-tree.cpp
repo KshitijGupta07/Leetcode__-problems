5fcba57eba150236e490f1839b96870f2c191a51
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-01 17:39:24 +0530

    Time: 4 ms (98.23%) | Memory: 17.6 MB (18.98%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,25 +9,25 @@
  */
 class Solution {
 public:
-   TreeNode* solve(TreeNode* &root,TreeNode* p,TreeNode* q){
+   TreeNode* solve(TreeNode* root,TreeNode* &p,TreeNode* &q){
     if(root==NULL){
         return NULL;
     }
-       if(root==p||root==q){
-        return root;
-       }
-       TreeNode* l=solve(root->left,p,q);
-       TreeNode* r=solve(root->right,p,q);
-       if(l!=NULL&&r!=NULL){
-        return root;
-       }
-       if(l==NULL&&r!=NULL){
-        return r;
-       }
-       if(l!=NULL&&r==NULL){
-        return l;
-       }
-       return NULL;
+         if(root==p||root==q){
+            return root;
+         }
+         TreeNode* left=solve(root->left,p,q);
+         TreeNode* right=solve(root->right,p,q);
+         if(left==NULL&&right==NULL){
+            return NULL;
+         }
+         if(left==NULL&&right!=NULL){
+            return right;
+         }
+         if(left!=NULL&&right==NULL){
+            return left;
+         }
+         return root;
    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         TreeNode* ans=solve(root,p,q);
