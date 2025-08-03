0fece6628802f276ed289df8d96b1c84515b93fe
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-04 00:12:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.07%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-     long long  power(long long  a,long long n){
-    int element=pow(10,9)+7;
-         if(n==0){
-             return 1;
-         }
-         if(n%2==0){
-             long long ans=power(a,n/2)%element;
-             return (ans%element*ans%element);
-         }
-       long long   ans=power(a,n-1)%element;
-         return (ans*a)%element;
-     }
-    int countGoodNumbers(long long n) {
-        int element=(pow(10,9)+7);
-        if(n%2==0){
-            return (power(20,n/2))%element;
+    long long  solve(long long  a,long long  x){
+        if(x==0){
+            return 1;
         }
-        return (power(5,n/2 +1)*power(4,n/2))%element;
+        if(x==1){
+            return a;
+        }
+        long long  element=pow(10,9)+7;
+
+        if(x%2==0){
+            long long  c=solve(a,x/2)%element;
+            return( c%element * c%element)%element;
+        }
+        long long  b=(a*solve(a,x-1)%element)%element;
+        return b%element;
+    }
+    int countGoodNumbers(long long n) {
+        int element=pow(10,9)+7;
+        long long  ans=solve(4,n/2)%element;
+        long long   ans2=solve(5,n-n/2)%element;
+        
+        return (ans%element*ans2%element)%element;
     }
 };
