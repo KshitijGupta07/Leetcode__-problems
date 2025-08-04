d96df913abbaae4216ac15a0441488b507c4f886
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-04 19:05:20 +0530

    Time: 3 ms (70.56%) | Memory: 10.8 MB (92.01%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -3,27 +3,23 @@ public:
     int characterReplacement(string s, int k) {
         int l=0;
         int r=0;
-        vector<int>hash(26,0);
-        int maxlen=0;
+        int ans=0;
         int maxf=0;
+        vector<int>hash(26,0);
         while(r<s.length()){
-            hash[s[r]-'A']++;
+           hash[s[r]-'A']+=1;
             maxf=max(maxf,hash[s[r]-'A']);
-            if(r-l+1-maxf>k){
-                hash[s[l]-'A']--;
-                maxf=0;
-                for(int i=0;i<26;i++){
-                    maxf=max(maxf,hash[i]);
-                }
-                
+            while(r-l+1-maxf>k){
+                hash[s[l]-'A']-=1;
                 l++;
+                for(auto j:hash){
+                    maxf=max(maxf,j);
+                }
             }
-            else{
-                maxlen=max(maxlen,r-l+1);
-                
-            }
+            ans=max(ans,r-l+1);
             r++;
+            
         }
-        return maxlen;
+        return ans;
     }
 };
