be274d7c2d215f35f76fecb83ab01cb4a6eb5b70
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-05 17:44:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (44.06%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        map<char,int>m;
-    
+        vector<int>hash1(256,0);
         for(int i=0;i<t.length();i++){
-            m[t[i]]++;
+            hash1[t[i]-'0']+=1;
         }
-        
-        int length=INT_MAX;
-        int j=0;
         int count=t.length();
-        int x=0;
-        int y=0;
-        while(y<s.length()){
-            if(m[s[y]]>0){
-                count--;
-  
+        int l=0;
+        int r=0;
+        int start=-1;
+        int end=-1;
+        int length=INT_MAX;
+        while(r<s.length()){
+            if(hash1[s[r]-'0']>0){
+                count-=1;
             }
-            m[s[y]]--;
-            y++;
-            if(count==0){
-                while(count==0){
-                    if(y-x<length){
-                        length=y-x;
-                        j=x;
-                    }
-                    m[s[x]]++;
-                    if(m[s[x]]>0){
-                        count++;
-                    }
-                    x++;
+            hash1[s[r]-'0']-=1;
+            while(count==0){
+                if(length>r-l+1){
+                    length=r-l+1;
+                    start=l;
+                    end=r;
                 }
+                hash1[s[l]-'0']+=1;
+                
+                if(hash1[s[l]-'0']>0){
+                    count++;
+                }
+                l++;
             }
-
+            r++;
         }
-        if(length==INT_MAX){
+        if(start==-1){
             return "";
         }
-        return s.substr(j,length);
+        return s.substr(start,end-start+1);
+
     }
 };
