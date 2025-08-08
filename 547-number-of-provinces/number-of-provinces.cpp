dbf72f2a830325890c220993948a064465e7d50e
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-08 16:10:19 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (29.80%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-   void dfs(vector<vector<int>>&graph,vector<bool>&visited,int i){
-    visited[i]=true;
-    for(auto j:graph[i]){
-        if(visited[j]==false){
-            dfs(graph,visited,j);
-        }
-    }
-   }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        vector<vector<int>>graph(isConnected.size());
-        for(int i=0;i<isConnected.size();i++){
-            for(int j=0;j<isConnected[i].size();j++){
-                if(isConnected[i][j]==1){
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
-                }
+    void dfs(int i,vector<vector<int>>&graph,vector<bool>&visited){
+        visited[i]=true;
+        for(auto j:graph[i]){
+            if(visited[j]==false){
+                dfs(j,graph,visited);
             }
         }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        int count=0;
         vector<bool>visited(n,false);
+        vector<vector<int>>graph(n);
+        for(int i=0;i<isConnected.size();i++){
+             for(int j=0;j<isConnected.size();j++){
+                  if(isConnected[i][j]==1){
+                       graph[i].push_back(j);
+                       graph[j].push_back(i);
+                  }
+             }
+
+        }
+        int count=0;
         for(int i=0;i<n;i++){
             if(visited[i]==false){
-                dfs(graph,visited,i);
-                count++;
+                dfs(i,graph,visited);
+                count+=1;
             }
         }
         return count;
