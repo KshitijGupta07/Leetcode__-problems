1b1ba49e6b260091c67853ebeb455d112fb67008
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-08 17:55:04 +0530

    Time: 137 ms (5.00%) | Memory: 52.5 MB (8.73%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
- 
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-           vector<vector<int>>visited(mat.size(),vector<int>(mat[0].size(),0));
-         queue<pair<int,int>>q;
-         vector<int>a{1,-1,0,0};
-         vector<int>b{0,0,1,-1};
-           for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[i].size();j++){
-                if(mat[i][j]==1){
-                    
-                    mat[i][j]=INT_MAX;
+        queue<pair<int,int>>q;
+           map<pair<int,int>,bool>m;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(mat[i][j]==0){
+                    q.push({i,j});
+                    m[{i,j}]=true;
+
                 }
                 else{
-                    q.push({i,j});
+                    mat[i][j]=INT_MAX;
                 }
             }
-           }
-           while(q.empty()==false){
-            pair<int,int>front=q.front();
-            q.pop();
-             int x=front.first;
-             int y=front.second;
+        }
+        vector<int>dx{-1,1,0,0};
+        vector<int>dy{0,0,-1,1};
+        while(q.empty()==false){
+            int x=q.front().first;
+            int y=q.front().second;
+q.pop();
              for(int i=0;i<4;i++){
-                if(x+a[i]>=0&&x+a[i]<mat.size()&&y+b[i]>=0&&y+b[i]<mat[0].size()&&mat[x+a[i]][y+b[i]]>1+mat[x][y]&&visited[x+a[i]][y+b[i]]==0){
-                    mat[x+a[i]][y+b[i]]=1+mat[x][y];
-                    visited[x+a[i]][y+b[i]]=1;
-                    q.push({x+a[i],y+b[i]});
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(nx>=0&&ny>=0&&nx<mat.size()&&ny<mat[0].size()&&m[{nx,ny}]==false&&mat[nx][ny]>1+mat[x][y]){
+                    mat[nx][ny]=1+mat[x][y];
+                    q.push({nx,ny});
+                    m[{nx,ny}]=true;
                 }
              }
-           }
-           return mat;
+        }
+        return mat;
+
     }
 };
