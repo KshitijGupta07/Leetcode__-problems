a06232bf00ca2099dde979113090e70ae3d779ea
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-10 00:46:16 +0530

    Time: 167 ms (20.81%) | Memory: 217.5 MB (21.08%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-   bool solve(vector<int>&stones,int i,int k,map<int,int>&m,vector<vector<int>>&dp){
-     if(i==stones.size()-1){
+   bool solve(vector<int>&stones,map<int,int>&m,int i,int k,vector<vector<int>>&dp){
+    if(i==stones.size()-1){
         return true;
-     }
-     if(dp[i][k]!=-1){
+    }
+    
+    if(dp[i][k]!=-1){
         return dp[i][k];
-     }
-     bool a=false;
-     bool b=false;
-     bool c=false;
-     
+    }
+    bool a=false;
+    bool b=false;
+    bool c=false;
       if(k-1>0&&m.find(stones[i]+k-1)!=m.end()){
-        a= solve(stones,m[stones[i]+k-1],k-1,m,dp);
+        a=solve(stones,m,m[stones[i]+k-1],k-1,dp);
       }
       if(m.find(stones[i]+k)!=m.end()){
-        b= solve(stones,m[stones[i]+k],k,m,dp);
+        b=solve(stones,m,m[stones[i]+k],k,dp);
       }
       if(m.find(stones[i]+k+1)!=m.end()){
-        c= solve(stones,m[stones[i]+k+1],k+1,m,dp);
+        c=solve(stones,m,m[stones[i]+k+1],k+1,dp);
       }
-
       return dp[i][k]=a||b||c;
-     
-     
-         
    }
     bool canCross(vector<int>& stones) {
-    if(stones[1]!=1){
+        if(stones[1]>1){
             return false;
         }
-        int last =0;
         map<int,int>m;
-        for(int i =1;i<stones.size();i++){
-            m[stones[i]]=i;
+        int count=0;
+        for(auto j:stones){
+            m[j]=count;
+            count++;
         }
         vector<vector<int>>dp(stones.size(),vector<int>(stones.size(),-1));
-        bool ans=solve(stones,1,1,m,dp);
+        int i=0;
+        bool ans=solve(stones,m,1,1,dp);
         return ans;
     }
 };
