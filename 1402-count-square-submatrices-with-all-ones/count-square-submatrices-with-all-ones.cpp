9a75ecba6d934dc970dd68cf8bd96299c7e8710f
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-12 16:12:34 +0530

    Time: 567 ms (5.00%) | Memory: 47.9 MB (6.04%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>&matrix,int i,int j,map<pair<int,int>,bool>&m,vector<vector<int>>&dp){
+        if(i>=matrix.size()||j>=matrix[0].size()||matrix[i][j]==0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int x=solve(matrix,i+1,j,m,dp)+1;
+        int y=solve(matrix,i,j+1,m,dp)+1;
+        int z=solve(matrix,i+1,j+1,m,dp)+1;
+        return dp[i][j]=min(x,min(y,z));
+    }
     int countSquares(vector<vector<int>>& matrix) {
-        vector<vector<int>>dp(matrix.size(),vector<int>(matrix[0].size(),0));
-        for(int i=0;i<matrix.size();i++){
-            dp[i][0]=matrix[i][0];
-        }
-        for(int j=0;j<matrix[0].size();j++){
-            dp[0][j]=matrix[0][j];
-        }
-        for(int i=1;i<matrix.size();i++){
-            for(int j=1;j<matrix[0].size();j++){
-                if(matrix[i][j]==0){
-                    dp[i][j]=0;
-                }
-                else{
-                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
-                }
-            }
-        }
         int ans=0;
-        for(auto j:dp){
-            for(auto k:j){
-                ans+=k;
+        map<pair<int,int>,bool>m;
+        vector<vector<int>>dp(matrix.size(),vector<int>(matrix[0].size(),-1));
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]==1&&m[{i,j}]==false){
+                    int a=solve(matrix,i,j,m,dp);
+                    cout<<a<<endl;
+                    ans=ans+a;
+                }
             }
         }
         return ans;
