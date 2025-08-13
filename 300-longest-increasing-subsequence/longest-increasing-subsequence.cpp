db0c819a5c8655501fff86349753289e49828fb2
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-13 22:39:34 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (88.08%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-   int solve(vector<int>&nums,int curr,int prev,vector<vector<int>>&dp){
-      if(curr>=nums.size()){
-        return 0;
-      }
-      if(dp[curr][prev+1]!=-1){
-        return dp[curr][prev+1];
-      }
-      int exclude=solve(nums,curr+1,prev,dp);
-      int include=0;
-      if(prev==-1||nums[curr]>nums[prev]){
-        include=1+solve(nums,curr+1,curr,dp);
-      }
-      
-      dp[curr][prev+1]= max(include,exclude);
-      
-      return dp[curr][prev+1];
-
-   }
-   int solvetab(vector<int>&nums){
-     vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,0));
-      for(int i=nums.size()-1;i>=0;i--){
-        for(int j=i-1;j>=-1;j--){
-            int include=0;
-            if(j==-1||nums[i]>nums[j]){
-                include=1+dp[i+1][i];
+    int binarysearch(vector<int>&ans,int target){
+         int start=0;
+         int end=ans.size()-1;
+         int x=0;
+         while(start<=end){
+            int mid=(start+end)/2 ;
+            if(ans[mid]>=target){
+                  x=mid;
+                  end=mid-1;
             }
-            int exclude=dp[i+1][j];
-            dp[i][j]=max(include,exclude);
-
-        }
-      }
-      return dp[0][0];
-   }
+            else{
+                start=mid+1;
+            }
+         }
+         return x;
+    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
-        int ans=solve(nums,0,-1,dp);
-
-        return ans;
+        
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(ans.empty()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                if(ans.back()<nums[i]){
+                    ans.push_back(nums[i]);
+                }
+                else{
+                    int index=binarysearch(ans,nums[i]);
+                    ans[index]=nums[i];
+                }
+            }
+        }
+        return ans.size();
     }
 };
