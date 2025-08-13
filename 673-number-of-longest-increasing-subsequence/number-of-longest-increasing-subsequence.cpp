8b37a4d3e805356c243ae7912470fa5677702287
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-14 00:30:03 +0530

    Time: 43 ms (16.33%) | Memory: 17.6 MB (8.93%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -5,28 +5,27 @@ public:
         vector<int>count(nums.size(),1);
         for(int i=0;i<nums.size();i++){
             for(int j=i;j<nums.size();j++){
-                if(nums[j]>nums[i]){
+                  if(nums[j]>nums[i]){
                     if(dp[j]<dp[i]+1){
-                        count[j]=count[i];
                         dp[j]=dp[i]+1;
+                        count[j]=count[i];
                     }
                     else if(dp[j]==dp[i]+1){
                         count[j]+=count[i];
                     }
-                }
-                
+                  }
             }
-        }
-        int maxi=0;
-        for(auto j:dp){
-            maxi=max(maxi,j);
         }
         int ans=0;
-        for(int i=0;i<nums.size();i++){
-            if(dp[i]==maxi){
-                ans+=count[i];
+        for(auto j:dp){
+            ans=max(ans,j);
+        }
+        int counter=0;
+        for(int i=0;i<dp.size();i++){
+            if(dp[i]==ans){
+                counter=counter +count[i];
             }
         }
-        return ans;
+        return counter;
     }
 };
