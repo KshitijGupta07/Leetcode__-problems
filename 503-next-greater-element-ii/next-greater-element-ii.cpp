09d3ab72dce8e92c9ee1be86519f2c4342368b6a
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-22 19:42:02 +0530

    Time: 13 ms (12.42%) | Memory: 31.1 MB (5.11%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int>ans;
-        stack<int>st;
-        int n=nums.size();
-        for(int i=2*n-1;i>=0;i--){
-            while(st.empty()==false&&st.top()<=nums[i%n]){
-                st.pop();
-            }
-            if(i<n){
-                if(st.empty()==true){
-                    ans.push_back(-1);
-                }
-                else{
-                    ans.push_back(st.top());
-                }
-            }
-            st.push(nums[i%n]);
-
+        vector<int>ans=nums;
+        for(int i=0;i<nums.size();i++){
+            ans.push_back(nums[i]);
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        vector<int>m;
         
+        stack<int>st;
+        st.push(-1);
+        for(int i=ans.size()-1;i>=0;i--){
+              while(st.top()!=-1&&ans[st.top()%nums.size()]<=nums[i%nums.size()]){
+                   st.pop();
+              }
+              if(i<nums.size()){
+              if(st.top()==-1){
+                  m.push_back(-1);
+              }
+              else{
+                m.push_back(nums[st.top()%nums.size()]);
+              }
+              }
+              st.push(i%nums.size());
+        }
+        reverse(m.begin(),m.end());
+        return m;
     }
 };
