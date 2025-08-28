132cd56f2f4175a02c9fab5d6b22e81e68047628
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-08-28 17:34:15 +0530

    Time: 119 ms (17.55%) | Memory: 185.6 MB (21.56%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,63 +1,64 @@
 class LRUCache {
 public:
-    class Node{
-        public:
-        int val;
-        int key;
-        Node* prev;
-        Node* next;
-        Node(int key,int val){
-            this->key=key;
-            this->val=val;
-        }
-
-    };
-    int count=0;
-    map<int,Node*>m;
-    Node* head= new Node(-1,-1);
-    Node* tail=new Node(-1,-1);
+   class Node{
+      public:
+      int key;
+      int val;
+      Node* left;
+      Node* right;
+      Node(int key,int val){
+        this->key=key;
+        this->val=val;
+      }
+   };
+   int count=0;
+   map<int,Node*>m;
+   Node* head=new Node(-1,-1);
+   Node* tail=new Node(-1,-1);
     LRUCache(int capacity) {
-        head->next=tail;
         count=capacity;
-        tail->prev=head;
+        head->right=tail;
+        tail->left=head;
     }
     
     int get(int key) {
-        if(m.find(key)==m.end()){
-            return -1;
-        }
-        Node* curr=m[key];
-        remove(curr);
-        insert(curr);
-        return curr->val;
+      if(m.find(key)==m.end()){
+        return -1;
+      }
+      Node* curr=m[key];
+       remove(curr);
+       insert(curr);
+       return curr->val;
     }
     void insert(Node* node){
-        Node* temp=head->next;
-        node->next=temp;
-        temp->prev=node;
-        node->prev=head;
-        head->next=node;
-
+        Node* temp=head->right;
+        head->right=node;
+        node->right=temp;
+        temp->left=node;
+        node->left=head;
+        
     }
-    void remove(Node* temp){
-        Node* p=temp->prev;
-        Node* n=temp->next;
-        p->next=n;
-        n->prev=p;
+    void remove(Node* node){
+        Node* temp=node->right;
+        node->right=NULL;
+        Node* temp2=node->left;
+        node->left=NULL;
+        temp2->right=temp;
+        temp->left=temp2;
     }
     void put(int key, int value) {
-        if(m.find(key)!=m.end()){
-            Node* c=m[key];
+         if(m.find(key)!=m.end()){
+            Node* x=m[key];
             m.erase(key);
-            remove(c);
-        }
-        if(m.size()==count){
-            m.erase(tail->prev->key);
-            remove(tail->prev);
-            
-        }
-        insert(new Node(key,value));
-        m[key]=head->next;
+            remove(x);
+         }
+         if(m.size()==count){
+            m.erase(tail->left->key);
+             remove(tail->left);
+         }
+         insert(new Node(key,value));
+         m[key]=head->right;
+
     }
 };
 
