3a3175f3150a4995fb45dd64c911932710665d1c
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-09-07 13:37:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (29.85%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<bool>v(256,false);
-        vector<bool>v2(256,false);
-        vector<char>m(256);
+     
+        map<char,char>m;
+        map<char,bool>c;
         for(int i=0;i<s.length();i++){
-            cout<<int(t[i])<<endl;
-             if(v[s[i]]==false){
-                v[s[i]]=true;
-                if(v2[t[i]]==false){
-                m[s[i]]=t[i];
-             }
-             else{
+           
+    
+          if(m[s[i]]-'0'>=0&&m[s[i]]!=t[i]){
                 return false;
-             }
-             }
-             else{
-               if(m[s[i]]!=t[i]){
-                  return false;
-               }
-             }
-            v2[t[i]]=true; 
+          }
+           if(c[t[i]]==true&&m[s[i]]!=t[i]){
+            return false;
+           }
+           if(c[t[i]]==false){
+                      m[s[i]]=t[i];
+                      c[t[i]]=true;
+           }
+           
+        
+            
         }
-        return true;
+           
+    
+    return true;
     }
 };
