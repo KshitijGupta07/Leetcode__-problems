fa503752ae7fbc758f76b0bf2f9fac7173f06562
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-09-07 17:46:16 +0530

    Time: 2 ms (81.33%) | Memory: 10.8 MB (29.51%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,67 +1,64 @@
 class Solution {
 public:
-  int kmp(string s,string b){
-       vector<int>lps(b.length(),0);
+  bool kps(string pattern,string t){
+    int n=pattern.length();
+    vector<int>lps(n,0);
        int i=1;
        int len=0;
-       while(i<b.length()){
-        if(b[i]==b[len]){
+       while(i<n){
+           if(pattern[i]==pattern[len]){
+        
             len++;
-            lps[i]=len;
-            i++;
-        }
-        else{
-            if(len!=0){
-                len=lps[len-1];
-            }
-            else{
-                 lps[i]=0;
-                 i++;
-            }
-        }
-       }
-       i=0;
-       int j=0;
-       while(i<s.length()&&j<b.length()){
-           if(s[i]==b[j]){
-            i++;
-            j++;
-            if(j==b.length()){
-                return true;
-            }
+              lps[i]=len;
+              i++;
+           }
+           else if(len>0){
+              len=lps[len-1];
            }
            else{
-            if(j!=0){
+               len=0;
+               i++;
+           }
+       }
+         
+         i=0;
+        int j=0;
+        int count=0;
+        while(i<t.length()){
+            if(t[i]==pattern[j]){
+                i++;
+                j++;
+                if(j==pattern.length()){
+                    return true;
+                }
+            }
+            else if(j>0){
                 j=lps[j-1];
             }
             else{
+                
                 i++;
             }
-       }
-
-       }
-       return false;
-  }
-    int repeatedStringMatch(string a, string b) {
-        if(a==b){
-            return 1;
         }
+       return false;
+   }
+    int repeatedStringMatch(string a, string b) {
+        string s=a;
         int count=1;
-          string s=a;
-          while(s.length()<b.length()){
-            s+=a;
-            count++;
-          }
-          if(s==b){
+        while(s.length()<b.length()){
+          s+=a;
+          count++;
+        }
+        if(s==b){
             return count;
-          }
-          if(kmp(s,b)){
+        }
+        if(kps(b,s)){
             return count;
-          }
-          if(kmp(s+a,b)){
+        }
+         if(kps(b,s+a)){
             return count+1;
-          }
-          return -1;
-
+         }
+        
+        return -1;
     }
 };
