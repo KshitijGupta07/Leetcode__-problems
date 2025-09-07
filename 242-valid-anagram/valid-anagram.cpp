c1e5b6a4228d579c8785fc1d00d053542203122c
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-09-07 11:51:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (26.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int>m;
-        
-        for(auto j:s){
-            m[j]++;
-        }
-        map<char,int>x;
-        for(auto j:t){
-            x[j]++;
-        }
-        if(m.size()!=x.size()){
-            return false;
-        }
-        for(auto j:x){
-            if(m[j.first]==j.second){
-                continue;
-            }
-            else{
+         if(s.length()!=t.length())return false;
+         vector<int>hash1(26,0);
+         vector<int>hash2(26,0);
+         for(int i=0;i<s.length();i++){
+              hash1[s[i]-'a']+=1;
+              hash2[t[i]-'a']+=1;
+         }
+         for(int i=0;i<26;i++){
+             if(hash1[i]!=hash2[i]){
                 return false;
-            }
-        }
-        return true;
+             }
+         }
+         return true;
     }
 };
