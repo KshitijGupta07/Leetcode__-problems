52311ae6ccd06e8b241c59dbb3d8ba494746a35d
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-09-12 17:37:14 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (95.16%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,126 +1,52 @@
 class Solution {
 public:
-    vector<int>majority(vector<int>&nums){
-         if(nums.size()==1){
-            return nums;
-        }
-        int element1=pow(10,9)+1;
-        int element2=pow(10,9)+1;
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(count==0){
-                element1=nums[i];
-                count=1;
-            }
-            else if(element1==nums[i]){
-                count++;
-            }
-            else{
-                count--;
-            }
-        }
-        count=0;
-        for(int i=0;i<nums.size();i++){
-               if(count==0){
-                if(nums[i]!=element1){
-                     element2=nums[i];
-                     count=1;
-                }
-               }
-               else if(element2==nums[i]){
-                count++;
-               }
-               else{
-                count--;
-               }
-        }
-         count=0;
-         int count2=0;
-         for(int i=0;i<nums.size();i++){
-            if(element1==nums[i]){
-                count++;
-            }
-            if(element2==nums[i]){
-                count2++;
-            }
-         }
-         vector<int>temp;
-         if(count>nums.size()/3){
-            temp.push_back(element1);
-         }
-         if(count2>nums.size()/3){
-            temp.push_back(element2);
-         }
-        return temp;
-    }
     vector<int> majorityElement(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums;
+       int element1=INT_MIN;
+       int element2=INT_MIN;
+       int count1=0;
+       int count2=0;
+       for(int i=0;i<nums.size();i++){
+        if(element1==nums[i]){
+            count1++;
         }
-        int element1=pow(10,9)+1;
-        int element2=pow(10,9)+1;
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(count==0){
-                element1=nums[i];
-                count=1;
-            }
-            else if(element1==nums[i]){
-                count++;
-            }
-            else{
-                count--;
-            }
+        else if(element2==nums[i]){
+            count2++;
         }
-        count=0;
-        for(int i=0;i<nums.size();i++){
-               if(count==0){
-                if(nums[i]!=element1){
-                     element2=nums[i];
-                     count=1;
-                }
-               }
-               else if(element2==nums[i]){
-                count++;
-               }
-               else{
-                count--;
-               }
+         else  if(count1==0){
+            element1=nums[i];
+            count1=1;
+    
+           }
+          else   if(count2==0){
+            element2=nums[i];
+            count2=1;
+            
+           }
+         else{
+            count1--;
+            count2--;
+         }
+       
+
+
+       } 
+       count1=0;
+       count2=0;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]==element1){
+            count1++;
         }
-        count=0;
-         int count2=0;
-         for(int i=0;i<nums.size();i++){
-            if(element1==nums[i]){
-                count++;
-            }
-            if(element2==nums[i]){
-                count2++;
-            }
-         }
-         vector<int>temp;
-         if(count>nums.size()/3){
-            temp.push_back(element1);
-         }
-         if(count2>nums.size()/3){
-            temp.push_back(element2);
-         }
-         reverse(nums.begin(),nums.end());
-         vector<int>temp2=majority(nums);
-         if(temp.size()==2){
-            return temp;
-         }
-         if(temp2.size()==2){
-            return temp2;
-         }
-          
-         if(temp.size()==1&&temp2.size()==1){
-            if(temp[0]!=temp2[0]){
-                temp.push_back(temp2[0]);
-            }
-         }
-         if(temp.size()==0){
-            return temp2;
-         }
-         return temp;
+        if(nums[i]==element2){
+            count2++;
+        }
+       }
+       vector<int>temp;
+       if(count1>nums.size()/3){
+        temp.push_back(element1);
+       }
+       if(count2>nums.size()/3){
+        temp.push_back(element2);
+       }
+       return temp;
     }
 };
