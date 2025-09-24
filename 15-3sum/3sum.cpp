af7bfd1e5f4ac9d63e3e32116e550ceec1e16bec
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-09-24 17:32:55 +0530

    Time: 79 ms (16.85%) | Memory: 28.9 MB (99.01%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        vector<int>temp;
         for(int i=0;i<nums.size()-2;i++){
             if(i>0&&nums[i]==nums[i-1]){
                 continue;
             }
-             int start=i+1;
-             int end=nums.size()-1;
-             while(start<end){
-                while(start-1>i&&start<nums.size()&&nums[start]==nums[start-1]){
-                    start++;
-                }
-                while(end+1<nums.size()-1&&end>=0&&nums[end]==nums[end+1]){
+            int target=-nums[i];
+            int start=i+1;
+            int end=nums.size()-1;
+              while(start<end){
+                 while(start!=i+1&&start<nums.size()&&nums[start]==nums[start-1]){
+                     start++;
+                 }
+                 while(end!=nums.size()-1&&end>=0&&nums[end]==nums[end+1]){
                     end--;
+                 }
+                 if(start>=end){
+                    break;
+                 }
+                if(nums[start]+nums[end]==target){
+                     ans.push_back({nums[i],nums[start],nums[end]});
+                     start++;
+                     end--;
                 }
-                if(start<end){
-                if(nums[start]+nums[end]+nums[i]==0){
-                    ans.push_back({nums[i],nums[start],nums[end]});
-                    start++;
-                    end--;
-                }
-                else if(nums[start]+nums[end]+nums[i]>0){
+                else if(nums[start]+nums[end]>target){
                     end--;
                 }
                 else{
                     start++;
                 }
-                }
-             }
-             
+              }
         }
         return ans;
     }
-    
-
 };
