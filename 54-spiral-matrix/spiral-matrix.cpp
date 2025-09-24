9c2b3db274de3f3c091a7d6dfe169c4f0cb971a1
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-09-24 15:43:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (79.04%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
-   void solve(vector<vector<int>>&matrix,int& i,int& j,int& m,int& n,vector<int>&ans){
-    if(i>m||j>n){
-        return;
+    void solve(vector<int>&ans,int i,int j,int ri,int ci,vector<vector<int>>&matrix){
+       
+        for(int k=j;k<=ci;k++){
+            ans.push_back(matrix[i][k]);
+        }
+        i+=1;
+         if(i>ri||j>ci){
+            return;
+        }
+        for(int k=i;k<=ri;k++){
+            ans.push_back(matrix[k][ci]);
+        }
+        ci-=1;
+         if(i>ri||j>ci){
+            return;
+        }
+        for(int k=ci;k>=j;k--){
+            ans.push_back(matrix[ri][k]);
+        }
+        ri-=1;
+         if(i>ri||j>ci){
+            return;
+        }
+        for(int k=ri;k>=i;k--){
+            ans.push_back(matrix[k][j]);
+        }
+        j+=1;
+         if(i>ri||j>ci){
+            return;
+        }
+        solve(ans,i,j,ri,ci,matrix);
+
+
     }
-    cout<<i<<" "<<j<<" "<<m<<" "<<n<<endl;
-     for(int x=j;x<=n;x++){
-        ans.push_back(matrix[i][x]);
-     }
-     i+=1;
-     for(int x=i;x<=m;x++){
-        ans.push_back(matrix[x][n]);
-     }
-     n-=1;
-       if(i>m||j>n){
-        return;
-    }
-     for(int x=n;x>=j;x--){
-        ans.push_back(matrix[m][x]);
-     }
-     m-=1;
-     for(int x=m;x>=i;x--){
-        ans.push_back(matrix[x][j]);
-     }
-     j+=1;
-     solve(matrix,i,j,m,n,ans);
-}
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int>ans;
-        int m=matrix.size()-1;
-        int n=matrix[0].size()-1;
-        int i=0;
-        int j=0;
-        solve(matrix,i,j,m,n,ans);
+        solve(ans,0,0,matrix.size()-1,matrix[0].size()-1,matrix);
         return ans;
     }
 };
