85acc4524bf6ed9ee80f79a3c01bde52d6f2ae96
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-09-25 15:22:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.04%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,28 +2,16 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n==0){
-            return 1.000;
+            return 1.0000;
         }
-        if(n==1){
-            return x;
+        if(n%2==0){
+            double ans=myPow(x,n/2);
+            return ans*ans;
         }
-        if(n==-1){
-            return 1.00/x;
+        if(n<0){
+        double ans=myPow(x,n+1);
+         return ans/x;
         }
-        
-        
-            if(n%2==0){
-                double  a=myPow(x,n/2);
-                return a*a;
-            }
-            
-                
-            
-                    return x*myPow(x,n-1);
-                
-
-            
-        
-     
+        return x*myPow(x,n-1);
     }
 };
