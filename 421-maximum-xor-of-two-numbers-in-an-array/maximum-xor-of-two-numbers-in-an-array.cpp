9b085ad2e78d9ae276d1e98d243ff0a02d717cd1
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-10-03 16:43:58 +0530

    Time: 588 ms (81.50%) | Memory: 245.7 MB (24.68%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,49 +1,76 @@
 class Solution {
 public:
-   struct Node{
-         Node* link[2];
-         
-
-         bool contain(int bit){
-           return link[bit]!=NULL;
-         }
-         void put(int bit,Node* node){
-            link[bit]=node;
-         }
-         Node* get(int bit){
-            return link[bit];
-         }
-        
+   class TrieNode{
+      private:
+      TrieNode* ch[2];
+      bool isend;
+      public:
+      TrieNode(){
+        for(int i=0;i<2;i++){
+            ch[i]=NULL;
+        }
+        isend=false;
+      }
+      TrieNode* get(int c){
+        return ch[c];
+      }
+      void set(int b,TrieNode* t){
+         ch[b]=t;
+      }
+      void put(){
+        isend=true;
+      }
+      bool check(){
+        return isend;
+      }
    };
-    int findMaximumXOR(vector<int>& nums) {
-        Node* root=new Node();
-        for(int i=0;i<nums.size();i++){
-            Node* node=root;
-            for(int j=30;j>=0;j--){
-                int bit=((nums[i]>>j)&1);
-                
-                if(node->contain(bit)==false){
-                    node->put(bit,new Node());
+   class Trie{
+      private:
+       TrieNode* root;
+       public:
+       Trie(){
+       root=new TrieNode();
+       }
+       void insert(int a){
+             TrieNode* temp=root;
+             for(int i=31;i>=0;i--){
+                if(temp->get((a>>i)&1)==NULL){
+                    temp->set((a>>i)&1,new TrieNode());
                 }
-                node=node->get(bit);
-            }
+                temp=temp->get((a>>i)&1);
+        
+             }
+             temp->put();
+
+       }
+       int search(int a){
+             TrieNode* temp=root;
+             int count=0;
+             int ans=0;
+              for(int i=31;i>=0;i--){
+                if(temp->get(1-((a>>i)&1))!=NULL){
+                    ans=ans+pow(2,i);
+                    temp=temp->get(1-((a>>i)&1));
+                }
+                else{
+                temp=temp->get((a>>i)&1);
+                }
+                
+             }
+             return ans;
+       }
+
+   };
+
+    int findMaximumXOR(vector<int>& nums) {
+        Trie* t=new Trie();
+        Trie* temp=t;
+        for(int i=0;i<nums.size();i++){
+               temp->insert(nums[i]);
         }
         int ans=0;
         for(int i=0;i<nums.size();i++){
-            Node* node=root;
-            int m=0;
-            for(int j=30;j>=0;j--){
-                int bit=((nums[i]>>j)&1);
-                 if(node->contain(1-bit)){
-                      m=m+(1<<j);
-                      node=node->get(1-bit);
-                 }
-                 else{
-                    node=node->get(bit);
-                 }
-
-            }
-            ans=max(ans,m);
+            ans=max(ans,temp->search(nums[i]));
         }
         return ans;
     }
