71cf3316e06bbeddce211d423e0c9a68e23a5f68
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-10-09 10:28:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (33.40%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int start=0;
-        int end=nums.size()-1;
-        while(start<end){
-            int mid=(start+end)/2 ;
-            if(mid>0&&mid<nums.size()-1&&nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
-                return mid;
-            }
-            else if(mid+1<nums.size()&&nums[mid]<nums[mid+1]){
-                start=mid+1;
-            }
-            else if(mid-1>=0&&nums[mid-1]>nums[mid]){
-                end=mid-1;
-            }
-            else if(mid==0){
-                if(nums[0]>nums[1]){
-                    return 0;
+        if(nums.size()==1){
+            return 0;
+        }
+         int start=0;
+         int end=nums.size()-1;
+         while(start<=end){
+            int mid=(start+end)>>1;
+            if(mid==0){
+                if(nums[mid]>nums[mid+1]){
+                    return mid;
                 }
                 else{
                     start=mid+1;
                 }
+                
             }
-            else if(mid==nums.size()-1){
-                if(nums[mid]>nums[mid-1]){
-                    return mid;
-                }
-                else{
-                    end=mid-1;
-                }
+           else if(mid==nums.size()-1){
+            if(nums[mid]>nums[mid-1]){
+                return mid;
             }
-        
-            
-        }
-        return start;
+              else{
+                end=mid-1;
+              }
+                
+            }
+            else if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid]<nums[mid+1]){
+                start=mid+1;
+            }
+            else {
+                end=mid-1;
+            }
+
+         }
+         return start;
     }
 };
