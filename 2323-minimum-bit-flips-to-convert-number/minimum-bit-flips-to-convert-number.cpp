ee873a1b99585af5201e2167f2e33e8cbb8b2f63
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-10-24 23:55:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (60.35%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int count =0;
-        while(start>0&&goal>0){
-            
-            cout<<(start&1)<<" "<<(goal&1)<<endl;
-            if((start&1)!=(goal&1)){
+        int xo=(start^goal);
+        int count=0;
+        while(xo>0){
+            if(xo%2==1){
                 count++;
             }
-            start=start>>1;
-            goal=goal>>1;
-        }
-        
-       cout<<count<<endl;
-        while(start>0){
-            if(start&1){
-                count++;
-            }
-            start=start>>1;
-        }
-        while(goal>0){
-            if(goal&1){
-                count++;
-            }
-            goal=goal>>1;
+            xo=xo>>1;
         }
         return count;
+
     }
 };
