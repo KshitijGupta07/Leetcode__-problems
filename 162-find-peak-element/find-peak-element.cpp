b8e37c32f3097ad065d697539a8c04eba88e6deb
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-10-25 10:32:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (34.14%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -4,39 +4,33 @@ public:
         if(nums.size()==1){
             return 0;
         }
-         int start=0;
-         int end=nums.size()-1;
-         while(start<=end){
-            int mid=(start+end)>>1;
-            if(mid==0){
-                if(nums[mid]>nums[mid+1]){
-                    return mid;
-                }
-                else{
-                    start=mid+1;
-                }
-                
+        int start=0;
+        int end=nums.size()-1;
+        while(start<=end){
+            int mid=(start+end)/2 ;
+            if(mid==0&&nums[mid]>nums[mid+1]){
+                return 0;
             }
-           else if(mid==nums.size()-1){
-            if(nums[mid]>nums[mid-1]){
+            else if(mid==nums.size()-1&&nums[mid]>nums[mid-1]){
                 return mid;
             }
-              else{
+             else if(mid==0&&nums[mid]<nums[mid+1]){
+                start=mid+1;
+            }
+            else if(mid==nums.size()-1&&nums[mid]<nums[mid-1]){
                 end=mid-1;
-              }
-                
             }
             else if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]){
                 return mid;
             }
-            else if(nums[mid]<nums[mid+1]){
-                start=mid+1;
-            }
-            else {
+           
+            else if(nums[mid]>nums[mid+1]){
                 end=mid-1;
             }
-
-         }
-         return start;
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
     }
 };
