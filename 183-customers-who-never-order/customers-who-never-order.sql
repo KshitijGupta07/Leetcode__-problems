b3f429b443ced9eec8691dff2831bfce63a81bb7
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-10-26 23:00:27 +0530

    Time: 566 ms (32.42%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT C.name as Customers FROM Customers C LEFT JOIN Orders O ON C.id=O.customerId WHERE O.id is NULL;
+SELECT C.name as Customers FROM Customers as C LEFT JOIN Orders as O ON C.id=O.customerId WHERE O.id IS NULL;
