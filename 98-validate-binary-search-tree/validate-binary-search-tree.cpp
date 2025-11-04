a592d261cb889b5294c925a4ecdcc5ccad89b185
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-11-04 09:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (94.69%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -12,24 +12,18 @@
 class Solution {
 public:
    bool solve(TreeNode* root,long long  mini,long long maxi){
-    
     if(root==NULL){
         return true;
     }
-      if(root->val>mini&&root->val<maxi){
-        cout<<root->val<<" "<<mini<<" "<<maxi<<endl;
-        long long element=root->val;
-        bool left=solve(root->left,mini,min(maxi,element));
-        bool right=solve(root->right,max(mini,element),maxi);
-        return left&&right;
-
-      }
-      return false;
+      bool left=solve(root->left,mini,root->val);
+      bool right=solve(root->right,root->val,maxi);
+      return left&&right&&(root->val>mini&&root->val<maxi);
    }
     bool isValidBST(TreeNode* root) {
-        long long  mini=INT_MIN-1LL;
-        long long  maxi=INT_MAX+1LL;
-        bool ans=solve(root,mini,maxi);
+        if(root==NULL){
+            return true;
+        }
+        bool ans=solve(root,INT_MIN-1LL,INT_MAX+1LL);
         return ans;
     }
 };
