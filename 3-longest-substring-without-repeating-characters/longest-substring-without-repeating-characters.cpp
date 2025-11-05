671807a60bca01a88630339255aed946c206e7b6
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-11-05 14:42:21 +0530

    Time: 20 ms (24.25%) | Memory: 13.6 MB (38.31%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        if(s.length()==1){
+            return 1;
+        }
         map<char,int>m;
         int l=0;
         int r=0;
         int ans=0;
         while(r<s.length()){
-            m[s[r]]++;
-            if(r-l+1==m.size()){
-                ans=max(ans,r-l+1);
-            }
-            while(r-l+1>m.size()){
-                m[s[l]]-=1;
-                if(m[s[l]]==0){
+              m[s[r]]+=1;
+              while(m[s[r]]>1){
+                 m[s[l]]-=1;
+                 if(m[s[l]]==0){
                     m.erase(s[l]);
-                }
-                l++;
-            }
-            r++;
+                 }
+                 l++;
+              }
+              ans=max(ans,r-l+1);
+              r++;
         }
+
         return ans;
     }
 };
