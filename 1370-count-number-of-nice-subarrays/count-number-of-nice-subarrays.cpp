d61b68f6a8ffda0b2927e3c26f6c6e80535c57b0
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-11-05 16:31:50 +0530

    Time: 1635 ms (5.07%) | Memory: 118.8 MB (5.20%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k){
-        map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            nums[i]=nums[i]%2;
-        }
-        
-        int element=0;
-        for(int i=0;i<nums.size();i++){
-           element+=nums[i];
-           mp[element]++;
-        }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        vector<int>prefix;
+        map<int,int>m;
         int count=0;
-      
-        for(auto j:mp){
-            if(j.first==k){
-                if(mp[0]!=0){
-                count+=j.second*mp[0];
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==1){
+                count++;
             }
-            
-                count+=j.second;
-            
-            
+            prefix.push_back(count);
         }
-        if(j.first>k&&mp[j.first-k]!=0){
-            count+=j.second*mp[j.first-k];
-        }
-        
+        m[0]=1;
+        count=0;
+        for(int i=0;i<nums.size();i++){
+            cout<<m[prefix[i]-k]<<endl;
+            count+=m[prefix[i]-k];
+            m[prefix[i]]++;
         }
         return count;
     }
