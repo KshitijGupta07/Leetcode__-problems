bf79d1fb2aebe0598fe86ec13d739cf859ebf447
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-11-05 14:51:53 +0530

    Time: 49 ms (24.11%) | Memory: 79.1 MB (10.57%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-       
-        int l=0;
-        int r=0;
-        map<int,int>m;
-        int ans=0;
-        while(r<fruits.size()){
-              m[fruits[r]]+=1;
-              if(m.size()==2){
-                ans=max(ans,r-l+1);
-              }
-              while(m.size()>2){
-                 m[fruits[l]]-=1;
-                 if(m[fruits[l]]==0){
-                     m.erase(fruits[l]);
-                 }
-                 l++;
-              }
-              r++;
-        }
-        if(ans==0){
-            return fruits.size();
-        }
-        return ans;
+         map<int,int>m;
+         int l=0;
+         int r=0;
+         int ans=0;
+         while(r<fruits.size()){
+             m[fruits[r]]++;
+             while(m.size()>2){
+                m[fruits[l]]-=1;
+                if(m[fruits[l]]==0){
+                    m.erase(fruits[l]);
+                }
+                l++;
+             }
+             ans=max(ans,r-l+1);
+             r++;
+
+         }
+         return ans;
     }
 };
