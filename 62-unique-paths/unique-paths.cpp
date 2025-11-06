999cf259ba9bda774a4258c052b066b69cc6fde6
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2025-11-06 14:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (18.37%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-  int solve(int m,int n,int i,int j,vector<vector<int>>&dp){
-    if(i>=m||j>=n){
-        return 0;
-    }
-    if(i==m-1&&j==n-1){
-        return 1;
-    }
-    if(dp[i][j]!=-1){
-        return dp[i][j];
-    }
-    return dp[i][j]=solve(m,n,i+1,j,dp)+solve(m,n,i,j+1,dp);
-  }
-    int uniquePaths(int m, int n) {
+    int solve(int m,int n){
+    
         vector<vector<int>>dp(m,vector<int>(n,0));
         dp[m-1][n-1]=1;
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
-                if(i+1<m&&j+1<n){
-                dp[i][j]=dp[i+1][j]+dp[i][j+1];
-                }
-                else{
+                 if(i+1<m&&j+1<n){
+                    dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                 }
+                 else{
                     dp[i][j]=1;
-                }
+                 }
             }
         }
-    
-       return dp[0][0];
+        return dp[0][0];
+        
+    }
+
+    int uniquePaths(int m, int n) {
+        int ans=solve(m,n);
+        return ans;
     }
 };
