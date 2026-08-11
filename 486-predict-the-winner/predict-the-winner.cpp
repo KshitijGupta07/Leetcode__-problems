6ad7c70415c25d6ebeb4f7442ad094b7cdc01ac1
KshitijGupta07 <144730881+KshitijGupta07@users.noreply.github.com>
2026-08-12 00:19:44 +0530

    Time: 95 ms (14.08%) | Memory: 9.5 MB (85.81%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    bool solve(vector<int>&nums,int i,int j,int sum1,int sum2,int bit){
-        cout<<sum1<<" "<<sum2<<endl;
+    int solve(vector<int>&nums,int i,int j){
         if(i==j){
-            if(bit==0){
-                sum1+=nums[i];
-            }
-            else{
-                sum2+=nums[j];
-            }
-            return sum1>=sum2;
+            return nums[i];
         }
-        
-        bool x=false;
-        if(bit==0){
-            x=solve(nums,i+1,j,sum1+nums[i],sum2,bit^1)||solve(nums,i,j-1,sum1+nums[j],sum2,bit^1);
-        }
-        else{
-            x=solve(nums,i+1,j,sum1,sum2+nums[i],bit^1)&&solve(nums,i,j-1,sum1,sum2+nums[j],bit^1);
-        }
-        return x;
+        int x=nums[i]-solve(nums,i+1,j);
+        int y=nums[j]-solve(nums,i,j-1);
+        return max(x,y);
     }
     bool predictTheWinner(vector<int>& nums) {
-        bool x=solve(nums,0,nums.size()-1,0,0,0);
-        return x;
+        
+        return solve(nums , 0 ,nums.size()-1)>=0;
     }
 };
